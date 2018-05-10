nhu2000/ihmc-open-robotics-software

Implemented a sensor noise simulator and added it to the sensor reader.
For simulation only.

ihmc-simulation-toolkit/src/main/java/us/ihmc/sensorProcessing/simulatedSensors/SimulatedSensorHolderAndReader.java
@@ -2,8 +2,10 @@ package us.ihmc.sensorProcessing.simulatedSensors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.ejml.data.DenseMatrix64F;
 
 import us.ihmc.commons.Conversions;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
@@ -37,11 +39,19 @@ public class SimulatedSensorHolderAndReader implements SensorReader
    protected final List<Pair<IMUDefinition, Vector3DProvider>> linearAccelerationSensors = new ArrayList<>();
    protected final List<Pair<ForceSensorDefinition, WrenchCalculatorInterface>> forceTorqueSensors = new ArrayList<>();
 
+   protected final Random random = new Random(343465);
+   protected final SensorNoiseSimulator sensorNoiseSimulator;
    protected final SensorProcessing sensorProcessing;
 
    public SimulatedSensorHolderAndReader(StateEstimatorSensorDefinitions stateEstimatorSensorDefinitions,
-         SensorProcessingConfiguration sensorProcessingConfiguration, YoDouble yoTime, YoVariableRegistry parentRegistry)
+                                         SensorProcessingConfiguration sensorProcessingConfiguration, YoDouble yoTime, YoVariableRegistry parentRegistry)
    {
+
+      SensorNoiseParameters sensorNoiseParameters = sensorProcessingConfiguration.getSensorNoiseParameters();
+      if (sensorNoiseParameters != null)
+         this.sensorNoiseSimulator = new SensorNoiseSimulator(stateEstimatorSensorDefinitions, random, sensorNoiseParameters, registry);
+      else
+         this.sensorNoiseSimulator = null;
       this.sensorProcessing = new SensorProcessing(stateEstimatorSensorDefinitions, sensorProcessingConfiguration, registry);
       this.yoTime = yoTime;
       step.set(29831);
@@ -97,10 +107,74 @@ public class SimulatedSensorHolderAndReader implements SensorReader
 
    public void read()
    {
+      updateSensorNoiseSimulator();
+      updateSensorProcessor();
+      step.increment();
+   }
+
+   private void updateSensorNoiseSimulator()
+   {
+      if (sensorNoiseSimulator == null)
+         return;
+
       for (int i = 0; i < jointPositionSensors.size(); i++)
       {
          OneDoFJoint oneDoFJoint = jointPositionSensors.get(i).getLeft();
          double newValue = jointPositionSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setJointPositionSensorValue(oneDoFJoint, newValue);
+      }
+
+      for (int i = 0; i < jointTorqueSensors.size(); i++)
+      {
+         OneDoFJoint oneDoFJoint = jointTorqueSensors.get(i).getLeft();
+         double newValue = jointTorqueSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setJointTauSensorValue(oneDoFJoint, newValue);
+      }
+
+      for (int i = 0; i < jointVelocitySensors.size(); i++)
+      {
+         double newValue = jointVelocitySensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setJointVelocitySensorValue(jointVelocitySensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < orientationSensors.size(); i++)
+      {
+         QuaternionReadOnly newValue = orientationSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setOrientationSensorValue(orientationSensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < angularVelocitySensors.size(); i++)
+      {
+         Vector3DReadOnly newValue = angularVelocitySensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setAngularVelocitySensorValue(angularVelocitySensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < linearAccelerationSensors.size(); i++)
+      {
+         Vector3DReadOnly newValue = linearAccelerationSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setLinearAccelerationSensorValue(linearAccelerationSensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < forceTorqueSensors.size(); i++)
+      {
+         final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
+         forceTorqueSensor.calculate();
+         sensorNoiseSimulator.setForceSensorValue(forceTorqueSensors.get(i).getLeft(), forceTorqueSensor.getWrench());
+      }
+
+      sensorNoiseSimulator.startComputation();
+   }
+
+   private final DenseMatrix64F newWrench = new DenseMatrix64F(6, 1);
+
+   private void updateSensorProcessor()
+   {
+      for (int i = 0; i < jointPositionSensors.size(); i++)
+      {
+         OneDoFJoint oneDoFJoint = jointPositionSensors.get(i).getLeft();
+         double newValue = jointPositionSensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getJointPositionProcessedOutput(oneDoFJoint);
          sensorProcessing.setJointPositionSensorValue(oneDoFJoint, newValue);
       }
 
@@ -108,47 +182,69 @@ public class SimulatedSensorHolderAndReader implements SensorReader
       {
          OneDoFJoint oneDoFJoint = jointTorqueSensors.get(i).getLeft();
          double newValue = jointTorqueSensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getJointTauProcessedOutput(oneDoFJoint);
          sensorProcessing.setJointTauSensorValue(oneDoFJoint, newValue);
       }
 
       for (int i = 0; i < jointVelocitySensors.size(); i++)
       {
+         OneDoFJoint oneDoFJoint = jointVelocitySensors.get(i).getLeft();
          double newValue = jointVelocitySensors.get(i).getRight().getValue();
-         sensorProcessing.setJointVelocitySensorValue(jointVelocitySensors.get(i).getLeft(), newValue);
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getJointVelocityProcessedOutput(oneDoFJoint);
+         sensorProcessing.setJointVelocitySensorValue(oneDoFJoint, newValue);
       }
 
       for (int i = 0; i < orientationSensors.size(); i++)
       {
+         IMUDefinition imu = orientationSensors.get(i).getLeft();
          QuaternionReadOnly newValue = orientationSensors.get(i).getRight().getValue();
-         sensorProcessing.setOrientationSensorValue(orientationSensors.get(i).getLeft(), newValue);
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getIMUProcessedOutputs().get(i).getOrientationMeasurement();
+         sensorProcessing.setOrientationSensorValue(imu, newValue);
       }
 
       for (int i = 0; i < angularVelocitySensors.size(); i++)
       {
          Vector3DReadOnly newValue = angularVelocitySensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getIMUProcessedOutputs().get(i).getAngularVelocityMeasurement();
          sensorProcessing.setAngularVelocitySensorValue(angularVelocitySensors.get(i).getLeft(), newValue);
       }
 
       for (int i = 0; i < linearAccelerationSensors.size(); i++)
       {
          Vector3DReadOnly newValue = linearAccelerationSensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getIMUProcessedOutputs().get(i).getLinearAccelerationMeasurement();
          sensorProcessing.setLinearAccelerationSensorValue(linearAccelerationSensors.get(i).getLeft(), newValue);
       }
 
       for (int i = 0; i < forceTorqueSensors.size(); i++)
       {
-         final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
-         forceTorqueSensor.calculate();
-         sensorProcessing.setForceSensorValue(forceTorqueSensors.get(i).getLeft(), forceTorqueSensor.getWrench());
+         ForceSensorDefinition forceSensor = forceTorqueSensors.get(i).getLeft();
+
+         if (sensorNoiseSimulator == null)
+         {
+            final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
+            forceTorqueSensor.calculate();
+            newWrench.set(forceTorqueSensor.getWrench());
+         }
+         else
+         {
+            sensorNoiseSimulator.getForceSensorProcessedOutputs().getForceSensorValue(forceSensor, newWrench);
+         }
+
+         sensorProcessing.setForceSensorValue(forceSensor, newWrench);
       }
 
       long timestamp = Conversions.secondsToNanoseconds(yoTime.getDoubleValue());
       sensorProcessing.startComputation(timestamp, timestamp, -1);
-
-      step.increment();
    }
 
-   @Override public AuxiliaryRobotData newAuxiliaryRobotDataInstance()
+   @Override
+   public AuxiliaryRobotData newAuxiliaryRobotDataInstance()
    {
       return null;
    }

dbdxnuliba/IHMC-

ihmc-simulation-toolkit/src/main/java/us/ihmc/sensorProcessing/simulatedSensors/SimulatedSensorHolderAndReader.java
@@ -2,8 +2,10 @@ package us.ihmc.sensorProcessing.simulatedSensors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.ejml.data.DenseMatrix64F;
 
 import controller_msgs.msg.dds.AtlasAuxiliaryRobotData;
 import us.ihmc.commons.Conversions;
@@ -37,11 +39,19 @@ public class SimulatedSensorHolderAndReader implements SensorReader
    protected final List<Pair<IMUDefinition, Vector3DProvider>> linearAccelerationSensors = new ArrayList<>();
    protected final List<Pair<ForceSensorDefinition, WrenchCalculatorInterface>> forceTorqueSensors = new ArrayList<>();
 
+   protected final Random random = new Random(343465);
+   protected final SensorNoiseSimulator sensorNoiseSimulator;
    protected final SensorProcessing sensorProcessing;
 
    public SimulatedSensorHolderAndReader(StateEstimatorSensorDefinitions stateEstimatorSensorDefinitions,
-         SensorProcessingConfiguration sensorProcessingConfiguration, YoDouble yoTime, YoVariableRegistry parentRegistry)
+                                         SensorProcessingConfiguration sensorProcessingConfiguration, YoDouble yoTime, YoVariableRegistry parentRegistry)
    {
+
+      SensorNoiseParameters sensorNoiseParameters = sensorProcessingConfiguration.getSensorNoiseParameters();
+      if (sensorNoiseParameters != null)
+         this.sensorNoiseSimulator = new SensorNoiseSimulator(stateEstimatorSensorDefinitions, random, sensorNoiseParameters, registry);
+      else
+         this.sensorNoiseSimulator = null;
       this.sensorProcessing = new SensorProcessing(stateEstimatorSensorDefinitions, sensorProcessingConfiguration, registry);
       this.yoTime = yoTime;
       step.set(29831);
@@ -97,10 +107,74 @@ public class SimulatedSensorHolderAndReader implements SensorReader
 
    public void read()
    {
+      updateSensorNoiseSimulator();
+      updateSensorProcessor();
+      step.increment();
+   }
+
+   private void updateSensorNoiseSimulator()
+   {
+      if (sensorNoiseSimulator == null)
+         return;
+
       for (int i = 0; i < jointPositionSensors.size(); i++)
       {
          OneDoFJoint oneDoFJoint = jointPositionSensors.get(i).getLeft();
          double newValue = jointPositionSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setJointPositionSensorValue(oneDoFJoint, newValue);
+      }
+
+      for (int i = 0; i < jointTorqueSensors.size(); i++)
+      {
+         OneDoFJoint oneDoFJoint = jointTorqueSensors.get(i).getLeft();
+         double newValue = jointTorqueSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setJointTauSensorValue(oneDoFJoint, newValue);
+      }
+
+      for (int i = 0; i < jointVelocitySensors.size(); i++)
+      {
+         double newValue = jointVelocitySensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setJointVelocitySensorValue(jointVelocitySensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < orientationSensors.size(); i++)
+      {
+         QuaternionReadOnly newValue = orientationSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setOrientationSensorValue(orientationSensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < angularVelocitySensors.size(); i++)
+      {
+         Vector3DReadOnly newValue = angularVelocitySensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setAngularVelocitySensorValue(angularVelocitySensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < linearAccelerationSensors.size(); i++)
+      {
+         Vector3DReadOnly newValue = linearAccelerationSensors.get(i).getRight().getValue();
+         sensorNoiseSimulator.setLinearAccelerationSensorValue(linearAccelerationSensors.get(i).getLeft(), newValue);
+      }
+
+      for (int i = 0; i < forceTorqueSensors.size(); i++)
+      {
+         final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
+         forceTorqueSensor.calculate();
+         sensorNoiseSimulator.setForceSensorValue(forceTorqueSensors.get(i).getLeft(), forceTorqueSensor.getWrench());
+      }
+
+      sensorNoiseSimulator.startComputation();
+   }
+
+   private final DenseMatrix64F newWrench = new DenseMatrix64F(6, 1);
+
+   private void updateSensorProcessor()
+   {
+      for (int i = 0; i < jointPositionSensors.size(); i++)
+      {
+         OneDoFJoint oneDoFJoint = jointPositionSensors.get(i).getLeft();
+         double newValue = jointPositionSensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getJointPositionProcessedOutput(oneDoFJoint);
          sensorProcessing.setJointPositionSensorValue(oneDoFJoint, newValue);
       }
 
@@ -108,47 +182,69 @@ public class SimulatedSensorHolderAndReader implements SensorReader
       {
          OneDoFJoint oneDoFJoint = jointTorqueSensors.get(i).getLeft();
          double newValue = jointTorqueSensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getJointTauProcessedOutput(oneDoFJoint);
          sensorProcessing.setJointTauSensorValue(oneDoFJoint, newValue);
       }
 
       for (int i = 0; i < jointVelocitySensors.size(); i++)
       {
+         OneDoFJoint oneDoFJoint = jointVelocitySensors.get(i).getLeft();
          double newValue = jointVelocitySensors.get(i).getRight().getValue();
-         sensorProcessing.setJointVelocitySensorValue(jointVelocitySensors.get(i).getLeft(), newValue);
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getJointVelocityProcessedOutput(oneDoFJoint);
+         sensorProcessing.setJointVelocitySensorValue(oneDoFJoint, newValue);
       }
 
       for (int i = 0; i < orientationSensors.size(); i++)
       {
+         IMUDefinition imu = orientationSensors.get(i).getLeft();
          QuaternionReadOnly newValue = orientationSensors.get(i).getRight().getValue();
-         sensorProcessing.setOrientationSensorValue(orientationSensors.get(i).getLeft(), newValue);
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getIMUProcessedOutputs().get(i).getOrientationMeasurement();
+         sensorProcessing.setOrientationSensorValue(imu, newValue);
       }
 
       for (int i = 0; i < angularVelocitySensors.size(); i++)
       {
          Vector3DReadOnly newValue = angularVelocitySensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getIMUProcessedOutputs().get(i).getAngularVelocityMeasurement();
          sensorProcessing.setAngularVelocitySensorValue(angularVelocitySensors.get(i).getLeft(), newValue);
       }
 
       for (int i = 0; i < linearAccelerationSensors.size(); i++)
       {
          Vector3DReadOnly newValue = linearAccelerationSensors.get(i).getRight().getValue();
+         if (sensorNoiseSimulator != null)
+            newValue = sensorNoiseSimulator.getIMUProcessedOutputs().get(i).getLinearAccelerationMeasurement();
          sensorProcessing.setLinearAccelerationSensorValue(linearAccelerationSensors.get(i).getLeft(), newValue);
       }
 
       for (int i = 0; i < forceTorqueSensors.size(); i++)
       {
-         final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
-         forceTorqueSensor.calculate();
-         sensorProcessing.setForceSensorValue(forceTorqueSensors.get(i).getLeft(), forceTorqueSensor.getWrench());
+         ForceSensorDefinition forceSensor = forceTorqueSensors.get(i).getLeft();
+
+         if (sensorNoiseSimulator == null)
+         {
+            final WrenchCalculatorInterface forceTorqueSensor = forceTorqueSensors.get(i).getRight();
+            forceTorqueSensor.calculate();
+            newWrench.set(forceTorqueSensor.getWrench());
+         }
+         else
+         {
+            sensorNoiseSimulator.getForceSensorProcessedOutputs().getForceSensorValue(forceSensor, newWrench);
+         }
+
+         sensorProcessing.setForceSensorValue(forceSensor, newWrench);
       }
 
       long timestamp = Conversions.secondsToNanoseconds(yoTime.getDoubleValue());
       sensorProcessing.startComputation(timestamp, timestamp, -1);
-
-      step.increment();
    }
 
-   @Override public AtlasAuxiliaryRobotData newAuxiliaryRobotDataInstance()
+   @Override
+   public AtlasAuxiliaryRobotData newAuxiliaryRobotDataInstance()
    {
       return null;
    }