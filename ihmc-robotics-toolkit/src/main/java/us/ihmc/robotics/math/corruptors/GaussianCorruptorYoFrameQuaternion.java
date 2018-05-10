nhu2000/ihmc-open-robotics-software

Implemented a bunch of Gaussian corruptors.

ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/corruptors/GaussianCorruptorYoFrameQuaternion.java
@@ -1,0 +1,109 @@
+package us.ihmc.robotics.math.corruptors;
+
+import java.util.Random;
+
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameQuaternionReadOnly;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
+import us.ihmc.robotics.math.filters.ProcessingYoVariable;
+import us.ihmc.robotics.math.frames.YoFrameQuaternion;
+import us.ihmc.robotics.math.frames.YoFrameVector;
+import us.ihmc.yoVariables.providers.DoubleProvider;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+
+public class GaussianCorruptorYoFrameQuaternion extends YoFrameQuaternion implements ProcessingYoVariable
+{
+   private final Random random;
+   private final Vector3DReadOnly standardDeviation;
+   private final QuaternionReadOnly input;
+
+   private static Vector3DReadOnly convertStandardDeviationToVector(DoubleProvider standardDeviation)
+   {
+      return new Vector3DReadOnly()
+      {
+         @Override
+         public double getX()
+         {
+            return standardDeviation.getValue();
+         }
+
+         @Override
+         public double getY()
+         {
+            return standardDeviation.getValue();
+         }
+
+         @Override
+         public double getZ()
+         {
+            return standardDeviation.getValue();
+         }
+      };
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random, DoubleProvider standardDeviation,
+                                             ReferenceFrame referenceFrame)
+   {
+      this(namePrefix, nameSuffix, registry, random, convertStandardDeviationToVector(standardDeviation), null, referenceFrame);
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random, DoubleProvider standardDeviation,
+                                             FrameQuaternionReadOnly inputVariable)
+   {
+      this(namePrefix, nameSuffix, registry, random, convertStandardDeviationToVector(standardDeviation), inputVariable, inputVariable.getReferenceFrame());
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random,
+                                             Vector3DReadOnly standardDeviation, ReferenceFrame referenceFrame)
+   {
+      this(namePrefix, nameSuffix, registry, random, standardDeviation, null, referenceFrame);
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random,
+                                             Vector3DReadOnly standardDeviation, FrameQuaternionReadOnly inputVariable)
+   {
+      this(namePrefix, nameSuffix, registry, random, standardDeviation, inputVariable, inputVariable.getReferenceFrame());
+   }
+
+   private GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random,
+                                              Vector3DReadOnly standardDeviation, QuaternionReadOnly inputVariable, ReferenceFrame referenceFrame)
+   {
+      super(namePrefix, nameSuffix, referenceFrame, registry);
+      this.random = random;
+      input = inputVariable;
+
+      if (standardDeviation == null)
+         standardDeviation = new YoFrameVector(namePrefix + "StandardDeviation" + nameSuffix, referenceFrame, registry);
+      this.standardDeviation = standardDeviation;
+   }
+
+   @Override
+   public void update()
+   {
+      if (input == null)
+      {
+         throw new NullPointerException("GaussianCorruptorYoFrameQuaternion must be constructed with a non null "
+               + "input variable to call update(), otherwise use update(FrameQuaternionReadOnly)");
+      }
+
+      update(input);
+   }
+
+   public void update(FrameQuaternionReadOnly input)
+   {
+      checkReferenceFrameMatch(input);
+      update((QuaternionReadOnly) input);
+   }
+
+   private final Vector3D noiseRotationVector = new Vector3D();
+
+   public void update(QuaternionReadOnly input)
+   { // TODO Not sure if that's the proper way to generate axis-dependent noise.
+      noiseRotationVector.set(standardDeviation);
+      noiseRotationVector.scale(random.nextGaussian(), random.nextGaussian(), random.nextGaussian());
+      set(noiseRotationVector);
+      preMultiply(input);
+   }
+}

ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/corruptors/GaussianCorruptorYoFrameVector3D.java
@@ -1,0 +1,106 @@
+package us.ihmc.robotics.math.corruptors;
+
+import static us.ihmc.robotics.math.frames.YoFrameVariableNameTools.createXName;
+import static us.ihmc.robotics.math.frames.YoFrameVariableNameTools.createYName;
+import static us.ihmc.robotics.math.frames.YoFrameVariableNameTools.createZName;
+
+import java.util.Random;
+
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameTuple3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.robotics.math.filters.ProcessingYoVariable;
+import us.ihmc.robotics.math.frames.YoFrameTuple;
+import us.ihmc.robotics.math.frames.YoFrameVector;
+import us.ihmc.yoVariables.providers.DoubleProvider;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+
+public class GaussianCorruptorYoFrameVector3D extends YoFrameVector implements ProcessingYoVariable
+{
+   private final GaussianCorruptorYoVariable x, y, z;
+
+   private GaussianCorruptorYoFrameVector3D(GaussianCorruptorYoVariable x, GaussianCorruptorYoVariable y, GaussianCorruptorYoVariable z,
+                                            ReferenceFrame referenceFrame)
+   {
+      super(x, y, z, referenceFrame);
+
+      this.x = x;
+      this.y = y;
+      this.z = z;
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, DoubleProvider standardDeviation,
+                                                                                         ReferenceFrame referenceFrame)
+   {
+      GaussianCorruptorYoVariable x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, standardDeviation);
+      GaussianCorruptorYoVariable y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, standardDeviation);
+      GaussianCorruptorYoVariable z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, standardDeviation);
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, referenceFrame);
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, DoubleProvider standardDeviation, YoFrameTuple input)
+   {
+      GaussianCorruptorYoVariable x, y, z;
+      x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, standardDeviation, input.getYoX());
+      y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, standardDeviation, input.getYoY());
+      z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, standardDeviation, input.getYoZ());
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, input.getReferenceFrame());
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, Vector3DReadOnly standardDeviation,
+                                                                                         ReferenceFrame referenceFrame)
+   {
+      GaussianCorruptorYoVariable x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getX());
+      GaussianCorruptorYoVariable y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getY());
+      GaussianCorruptorYoVariable z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getZ());
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, referenceFrame);
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, Vector3DReadOnly standardDeviation, YoFrameTuple input)
+   {
+      GaussianCorruptorYoVariable x, y, z;
+      x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getX(), input.getYoX());
+      y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getY(), input.getYoY());
+      z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getZ(), input.getYoZ());
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, input.getReferenceFrame());
+   }
+
+   @Override
+   public void update()
+   {
+      x.update();
+      y.update();
+      z.update();
+   }
+
+   public void update(double xInput, double yInput, double zInput)
+   {
+      x.update(xInput);
+      y.update(yInput);
+      z.update(zInput);
+   }
+
+   public void update(Tuple3DReadOnly input)
+   {
+      x.update(input.getX());
+      y.update(input.getY());
+      z.update(input.getZ());
+   }
+
+   public void update(FrameTuple3DReadOnly inpput)
+   {
+      checkReferenceFrameMatch(inpput);
+      x.update(inpput.getX());
+      y.update(inpput.getY());
+      z.update(inpput.getZ());
+   }
+}

dbdxnuliba/IHMC-

ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/corruptors/GaussianCorruptorYoFrameQuaternion.java
@@ -1,0 +1,109 @@
+package us.ihmc.robotics.math.corruptors;
+
+import java.util.Random;
+
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameQuaternionReadOnly;
+import us.ihmc.euclid.tuple3D.Vector3D;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.euclid.tuple4D.interfaces.QuaternionReadOnly;
+import us.ihmc.robotics.math.filters.ProcessingYoVariable;
+import us.ihmc.yoVariables.providers.DoubleProvider;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoFrameQuaternion;
+import us.ihmc.yoVariables.variable.YoFrameVector3D;
+
+public class GaussianCorruptorYoFrameQuaternion extends YoFrameQuaternion implements ProcessingYoVariable
+{
+   private final Random random;
+   private final Vector3DReadOnly standardDeviation;
+   private final QuaternionReadOnly input;
+
+   private static Vector3DReadOnly convertStandardDeviationToVector(DoubleProvider standardDeviation)
+   {
+      return new Vector3DReadOnly()
+      {
+         @Override
+         public double getX()
+         {
+            return standardDeviation.getValue();
+         }
+
+         @Override
+         public double getY()
+         {
+            return standardDeviation.getValue();
+         }
+
+         @Override
+         public double getZ()
+         {
+            return standardDeviation.getValue();
+         }
+      };
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random, DoubleProvider standardDeviation,
+                                             ReferenceFrame referenceFrame)
+   {
+      this(namePrefix, nameSuffix, registry, random, convertStandardDeviationToVector(standardDeviation), null, referenceFrame);
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random, DoubleProvider standardDeviation,
+                                             FrameQuaternionReadOnly inputVariable)
+   {
+      this(namePrefix, nameSuffix, registry, random, convertStandardDeviationToVector(standardDeviation), inputVariable, inputVariable.getReferenceFrame());
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random,
+                                             Vector3DReadOnly standardDeviation, ReferenceFrame referenceFrame)
+   {
+      this(namePrefix, nameSuffix, registry, random, standardDeviation, null, referenceFrame);
+   }
+
+   public GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random,
+                                             Vector3DReadOnly standardDeviation, FrameQuaternionReadOnly inputVariable)
+   {
+      this(namePrefix, nameSuffix, registry, random, standardDeviation, inputVariable, inputVariable.getReferenceFrame());
+   }
+
+   private GaussianCorruptorYoFrameQuaternion(String namePrefix, String nameSuffix, YoVariableRegistry registry, Random random,
+                                              Vector3DReadOnly standardDeviation, QuaternionReadOnly inputVariable, ReferenceFrame referenceFrame)
+   {
+      super(namePrefix, nameSuffix, referenceFrame, registry);
+      this.random = random;
+      input = inputVariable;
+
+      if (standardDeviation == null)
+         standardDeviation = new YoFrameVector3D(namePrefix + "StandardDeviation" + nameSuffix, referenceFrame, registry);
+      this.standardDeviation = standardDeviation;
+   }
+
+   @Override
+   public void update()
+   {
+      if (input == null)
+      {
+         throw new NullPointerException("GaussianCorruptorYoFrameQuaternion must be constructed with a non null "
+               + "input variable to call update(), otherwise use update(FrameQuaternionReadOnly)");
+      }
+
+      update(input);
+   }
+
+   public void update(FrameQuaternionReadOnly input)
+   {
+      checkReferenceFrameMatch(input);
+      update((QuaternionReadOnly) input);
+   }
+
+   private final Vector3D noiseRotationVector = new Vector3D();
+
+   public void update(QuaternionReadOnly input)
+   { // TODO Not sure if that's the proper way to generate axis-dependent noise.
+      noiseRotationVector.set(standardDeviation);
+      noiseRotationVector.scale(random.nextGaussian(), random.nextGaussian(), random.nextGaussian());
+      setRotationVector(noiseRotationVector);
+      preMultiply(input);
+   }
+}

ihmc-robotics-toolkit/src/main/java/us/ihmc/robotics/math/corruptors/GaussianCorruptorYoFrameVector3D.java
@@ -1,0 +1,106 @@
+package us.ihmc.robotics.math.corruptors;
+
+import static us.ihmc.robotics.math.frames.YoFrameVariableNameTools.createXName;
+import static us.ihmc.robotics.math.frames.YoFrameVariableNameTools.createYName;
+import static us.ihmc.robotics.math.frames.YoFrameVariableNameTools.createZName;
+
+import java.util.Random;
+
+import us.ihmc.euclid.referenceFrame.ReferenceFrame;
+import us.ihmc.euclid.referenceFrame.interfaces.FrameTuple3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
+import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
+import us.ihmc.robotics.math.filters.ProcessingYoVariable;
+import us.ihmc.yoVariables.providers.DoubleProvider;
+import us.ihmc.yoVariables.registry.YoVariableRegistry;
+import us.ihmc.yoVariables.variable.YoFrameTuple3D;
+import us.ihmc.yoVariables.variable.YoFrameVector3D;
+
+public class GaussianCorruptorYoFrameVector3D extends YoFrameVector3D implements ProcessingYoVariable
+{
+   private final GaussianCorruptorYoVariable x, y, z;
+
+   private GaussianCorruptorYoFrameVector3D(GaussianCorruptorYoVariable x, GaussianCorruptorYoVariable y, GaussianCorruptorYoVariable z,
+                                            ReferenceFrame referenceFrame)
+   {
+      super(x, y, z, referenceFrame);
+
+      this.x = x;
+      this.y = y;
+      this.z = z;
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, DoubleProvider standardDeviation,
+                                                                                         ReferenceFrame referenceFrame)
+   {
+      GaussianCorruptorYoVariable x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, standardDeviation);
+      GaussianCorruptorYoVariable y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, standardDeviation);
+      GaussianCorruptorYoVariable z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, standardDeviation);
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, referenceFrame);
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, DoubleProvider standardDeviation, YoFrameTuple3D input)
+   {
+      GaussianCorruptorYoVariable x, y, z;
+      x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, standardDeviation, input.getYoX());
+      y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, standardDeviation, input.getYoY());
+      z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, standardDeviation, input.getYoZ());
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, input.getReferenceFrame());
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, Vector3DReadOnly standardDeviation,
+                                                                                         ReferenceFrame referenceFrame)
+   {
+      GaussianCorruptorYoVariable x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getX());
+      GaussianCorruptorYoVariable y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getY());
+      GaussianCorruptorYoVariable z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getZ());
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, referenceFrame);
+   }
+
+   public static GaussianCorruptorYoFrameVector3D createGaussianCorruptorYoFrameVector3D(String namePrefix, String nameSuffix, YoVariableRegistry registry,
+                                                                                         Random random, Vector3DReadOnly standardDeviation, YoFrameTuple3D input)
+   {
+      GaussianCorruptorYoVariable x, y, z;
+      x = new GaussianCorruptorYoVariable(createXName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getX(), input.getYoX());
+      y = new GaussianCorruptorYoVariable(createYName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getY(), input.getYoY());
+      z = new GaussianCorruptorYoVariable(createZName(namePrefix, nameSuffix), registry, random, () -> standardDeviation.getZ(), input.getYoZ());
+
+      return new GaussianCorruptorYoFrameVector3D(x, y, z, input.getReferenceFrame());
+   }
+
+   @Override
+   public void update()
+   {
+      x.update();
+      y.update();
+      z.update();
+   }
+
+   public void update(double xInput, double yInput, double zInput)
+   {
+      x.update(xInput);
+      y.update(yInput);
+      z.update(zInput);
+   }
+
+   public void update(Tuple3DReadOnly input)
+   {
+      x.update(input.getX());
+      y.update(input.getY());
+      z.update(input.getZ());
+   }
+
+   public void update(FrameTuple3DReadOnly inpput)
+   {
+      checkReferenceFrameMatch(inpput);
+      x.update(inpput.getX());
+      y.update(inpput.getY());
+      z.update(inpput.getZ());
+   }
+}