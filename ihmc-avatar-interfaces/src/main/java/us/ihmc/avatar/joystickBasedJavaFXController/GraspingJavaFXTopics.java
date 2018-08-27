4b9f7ad3a6b0a48607c081d1037f90e01043054c
InhoLee <ilee@ihmc.us>
2018-08-27 11:28:18 -0500

    Topics includes shapes parameters.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/joystickBasedJavaFXController/GraspingJavaFXTopics.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/joystickBasedJavaFXController/GraspingJavaFXTopics.java
@@ -13,8 +13,13 @@ public class GraspingJavaFXTopics
 
    private static final Category Root = apiFactory.createRootCategory(apiFactory.createCategoryTheme("Grasping"));
 
+   private static final CategoryTheme GraspingObject = apiFactory.createCategoryTheme("GraspingObject");
    private static final CategoryTheme FingerControl = apiFactory.createCategoryTheme("FingerControl");
 
+   private static final CategoryTheme Sphere = apiFactory.createCategoryTheme("Sphere");
+   private static final CategoryTheme Cylinder = apiFactory.createCategoryTheme("Cylinder");
+   private static final CategoryTheme Torus = apiFactory.createCategoryTheme("Torus");
+   private static final CategoryTheme Box = apiFactory.createCategoryTheme("Box");
    private static final CategoryTheme Left = apiFactory.createCategoryTheme("Left");
    private static final CategoryTheme Right = apiFactory.createCategoryTheme("Right");
    private static final CategoryTheme Thumb = apiFactory.createCategoryTheme("Thumb");
@@ -22,6 +27,11 @@ public class GraspingJavaFXTopics
    private static final CategoryTheme Middle = apiFactory.createCategoryTheme("Middle");
    private static final CategoryTheme Pinky = apiFactory.createCategoryTheme("Pinky");
 
+   private static final TypedTopicTheme<Double> Radius = apiFactory.createTypedTopicTheme("Radius");
+   private static final TypedTopicTheme<Double> Height = apiFactory.createTypedTopicTheme("Height");
+   private static final TypedTopicTheme<Double> TubeRadius = apiFactory.createTypedTopicTheme("TubeRadius");
+   private static final TypedTopicTheme<Double> Length = apiFactory.createTypedTopicTheme("Length");
+   private static final TypedTopicTheme<Double> Width = apiFactory.createTypedTopicTheme("Width");
    private static final TypedTopicTheme<Double> Roll = apiFactory.createTypedTopicTheme("Roll");
    private static final TypedTopicTheme<Double> Pitch = apiFactory.createTypedTopicTheme("Pitch");
    private static final TypedTopicTheme<Double> Pitch2 = apiFactory.createTypedTopicTheme("Pitch2");
@@ -29,6 +39,14 @@ public class GraspingJavaFXTopics
 
    private static final TypedTopicTheme<Integer> Selected = apiFactory.createTypedTopicTheme("Selected");
 
+   public static final Topic<Double> SphereRadius = Root.child(GraspingObject).child(Sphere).topic(Radius);
+   public static final Topic<Double> CylinderRadius = Root.child(GraspingObject).child(Cylinder).topic(Radius);
+   public static final Topic<Double> CylinderHeight = Root.child(GraspingObject).child(Cylinder).topic(Height);
+   public static final Topic<Double> TorusRadius = Root.child(GraspingObject).child(Torus).topic(Radius);
+   public static final Topic<Double> TorusTubeRadius = Root.child(GraspingObject).child(Torus).topic(TubeRadius);
+   public static final Topic<Double> BoxLength = Root.child(GraspingObject).child(Box).topic(Length);
+   public static final Topic<Double> BoxWidth = Root.child(GraspingObject).child(Box).topic(Width);
+   public static final Topic<Double> BoxHeight = Root.child(GraspingObject).child(Box).topic(Height);
    public static final Topic<Double> LeftThumbRoll = Root.child(FingerControl).child(Left).child(Thumb).topic(Roll);
    public static final Topic<Double> LeftThumb = Root.child(FingerControl).child(Left).child(Thumb).topic(Pitch);
    public static final Topic<Double> LeftThumb2 = Root.child(FingerControl).child(Left).child(Thumb).topic(Pitch2);
@@ -43,6 +61,8 @@ public class GraspingJavaFXTopics
    public static final Topic<Double> RightPinky = Root.child(FingerControl).child(Right).child(Pinky).topic(Pitch);
    public static final Topic<Boolean> LeftSendMessage = Root.child(FingerControl).child(Left).topic(SendMessage);
    public static final Topic<Boolean> RightSendMessage = Root.child(FingerControl).child(Right).topic(SendMessage);
+
+   public static final Topic<Integer> SelectedShape = Root.child(GraspingObject).topic(Selected);
 
    static
    {
