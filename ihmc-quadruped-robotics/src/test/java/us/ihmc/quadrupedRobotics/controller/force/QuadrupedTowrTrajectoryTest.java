2d6ea2734bec56c7484fcceeea223820f1a1b268
Romeo Orsolino <rorsolino@ihmc.us>
2018-09-11 21:09:58 -0500

    moved listenToTowr function out of the getSteps

--- ihmc-quadruped-robotics/src/test/java/us/ihmc/quadrupedRobotics/controller/force/QuadrupedTowrTrajectoryTest.java
+++ ihmc-quadruped-robotics/src/test/java/us/ihmc/quadrupedRobotics/controller/force/QuadrupedTowrTrajectoryTest.java
@@ -20,6 +20,7 @@ import us.ihmc.quadrupedRobotics.controller.QuadrupedControllerRequestedEvent;
 import us.ihmc.quadrupedRobotics.controller.QuadrupedSteppingStateEnum;
 import us.ihmc.quadrupedRobotics.input.managers.QuadrupedTeleopManager;
 import us.ihmc.quadrupedRobotics.model.QuadrupedInitialPositionParameters;
+import us.ihmc.quadrupedRobotics.planning.trajectoryConverter.QuadrupedTowrTrajectoryConverter;
 import us.ihmc.quadrupedRobotics.simulation.QuadrupedGroundContactModelType;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 import us.ihmc.ros2.Ros2Node;
@@ -257,6 +258,117 @@ public abstract class QuadrupedTowrTrajectoryTest implements QuadrupedMultiRobot
       assertTrue(variables.getRobotBodyZ().getDoubleValue() > 0.0);
       conductor.concludeTesting();
    }
+
+   @ContinuousIntegrationTest(estimatedDuration = 74.7)
+   @Test(timeout = 370000)
+   public void testQuadrupedTowrTrajectoryReplanning() throws BlockingSimulationRunner.SimulationExceededMaximumTimeException
+   {
+      PrintTools.info("Center of Mass and feet trajectory test");
+      QuadrupedTestBehaviors.standUp(conductor, variables);
+      QuadrupedTestBehaviors.startBalancing(conductor, variables, stepTeleopManager);
+
+      // let's have the robot go to a narrower step position
+
+      double stanceWidth = 0.5;
+      double stanceLength = 0.9;
+
+      Point3D frontLeftPosition = new Point3D(stanceLength / 2.0, stanceWidth / 2.0, 0.0);
+      Point3D frontRightPosition = new Point3D(stanceLength / 2.0, -stanceWidth / 2.0, 0.0);
+      Point3D backLeftPosition = new Point3D(-stanceLength / 2.0, stanceWidth / 2.0, 0.0);
+      Point3D backRightPosition = new Point3D(-stanceLength / 2.0, -stanceWidth / 2.0, 0.0);
+
+      QuadrupedTimedStepListMessage footsteps = new QuadrupedTimedStepListMessage();
+      footsteps.getQuadrupedStepList().add().set(QuadrupedMessageTools.createQuadrupedTimedStepMessage(RobotQuadrant.FRONT_LEFT, frontLeftPosition, 0.1, 0.0, 0.4));
+      footsteps.getQuadrupedStepList().add().set(QuadrupedMessageTools.createQuadrupedTimedStepMessage(RobotQuadrant.HIND_RIGHT, backRightPosition, 0.1, 0.3, 0.7));
+      footsteps.getQuadrupedStepList().add().set(QuadrupedMessageTools.createQuadrupedTimedStepMessage(RobotQuadrant.FRONT_RIGHT, frontRightPosition, 0.1, 0.6, 1.0));
+      footsteps.getQuadrupedStepList().add().set(QuadrupedMessageTools.createQuadrupedTimedStepMessage(RobotQuadrant.HIND_LEFT, backLeftPosition, 0.1, 0.9, 1.3));
+      footsteps.setIsExpressedInAbsoluteTime(false);
+
+      stepTeleopManager.pulishTimedStepListToController(footsteps);
+
+      YoBoolean shiftPlanBasedOnStepAdjustment = (YoBoolean) conductor.getScs().getVariable("shiftPlanBasedOnStepAdjustment");
+      YoBoolean useStepAdjustment = (YoBoolean) conductor.getScs().getVariable("useStepAdjustment");
+      YoBoolean flSpeedUp = (YoBoolean) conductor.getScs().getVariable("FrontLeftIsSwingSpeedUpEnabled");
+      YoBoolean frSpeedUp = (YoBoolean) conductor.getScs().getVariable("FrontRightIsSwingSpeedUpEnabled");
+      YoBoolean hlSpeedUp = (YoBoolean) conductor.getScs().getVariable("HindLeftIsSwingSpeedUpEnabled");
+      YoBoolean hrSpeedUp = (YoBoolean) conductor.getScs().getVariable("HindRightIsSwingSpeedUpEnabled");
+      shiftPlanBasedOnStepAdjustment.set(false);
+      useStepAdjustment.set(false);
+      flSpeedUp.set(false);
+      frSpeedUp.set(false);
+      hlSpeedUp.set(false);
+      hrSpeedUp.set(false);
+
+      conductor.addTerminalGoal(QuadrupedTestGoals.timeInFuture(variables, 2.0));
+      conductor.simulate();
+
+
+      Ros2Node ros2Node = ROS2Tools.createRos2Node(PubSubImplementation.INTRAPROCESS, "scripted_flat_ground_walking");
+      String robotName = quadrupedTestFactory.getRobotName();
+      MessageTopicNameGenerator controllerSubGenerator = QuadrupedControllerAPIDefinition.getSubscriberTopicNameGenerator(robotName);
+      MessageTopicNameGenerator controllerPubGenerator = QuadrupedControllerAPIDefinition.getPublisherTopicNameGenerator(robotName);
+
+      AtomicReference<QuadrupedControllerEnum> controllerState = new AtomicReference<>();
+      AtomicReference<QuadrupedSteppingStateEnum> steppingState = new AtomicReference<>();
+      ROS2Tools.createCallbackSubscription(ros2Node, QuadrupedControllerStateChangeMessage.class, controllerPubGenerator,
+                                           s -> controllerState.set(QuadrupedControllerEnum.fromByte(s.takeNextData().getEndQuadrupedControllerEnum())));
+
+      ROS2Tools.createCallbackSubscription(ros2Node, QuadrupedSteppingStateChangeMessage.class, controllerPubGenerator,
+                                           s -> steppingState.set(QuadrupedSteppingStateEnum.fromByte(s.takeNextData().getEndQuadrupedSteppingStateEnum())));
+
+      QuadrupedRequestedControllerStateMessage controllerMessage = new QuadrupedRequestedControllerStateMessage();
+      controllerMessage.setQuadrupedControllerRequestedEvent(QuadrupedControllerRequestedEvent.REQUEST_STEPPING.toByte());
+      IHMCROS2Publisher<QuadrupedRequestedControllerStateMessage> controllerStatePublisher = ROS2Tools.createPublisher(ros2Node, QuadrupedRequestedControllerStateMessage.class, controllerSubGenerator);
+      controllerStatePublisher.publish(controllerMessage);
+      conductor.addTerminalGoal(QuadrupedTestGoals.timeInFuture(variables, 1.0));
+      conductor.simulate();
+
+      boolean isTowrTrajectoryReceived = listenToTowr();
+      if(isTowrTrajectoryReceived)
+      {
+         IHMCROS2Publisher<State6d> flagPublisher = ROS2Tools.createPublisher(ros2Node, State6d.class, "stateMachineFlag");
+      }
+      QuadrupedTimedStepListMessage stepsMessage = getSteps();
+      IHMCROS2Publisher<QuadrupedTimedStepListMessage> timedStepPublisher = ROS2Tools.createPublisher(ros2Node, QuadrupedTimedStepListMessage.class, controllerSubGenerator);
+      timedStepPublisher.publish(stepsMessage);
+
+      CenterOfMassTrajectoryMessage comMessage = getCenterOfMassTrajectoryMessage();
+      IHMCROS2Publisher<CenterOfMassTrajectoryMessage> centerOfMassTrajectoryPublisher = ROS2Tools.createPublisher(ros2Node, CenterOfMassTrajectoryMessage.class, controllerSubGenerator);
+      centerOfMassTrajectoryPublisher.publish(comMessage);
+
+      QuadrupedBodyHeightMessage bodyHeightMessage = getBodyHeightMessage();
+      IHMCROS2Publisher<QuadrupedBodyHeightMessage> bodyHeightTrajectoryPublisher = ROS2Tools.createPublisher(ros2Node, QuadrupedBodyHeightMessage.class, controllerSubGenerator);
+      bodyHeightTrajectoryPublisher.publish((bodyHeightMessage));
+
+      conductor.addTerminalGoal(QuadrupedTestGoals.timeInFuture(variables, 10.0));
+      conductor.simulate();
+
+      stepsMessage = getSteps();
+      timedStepPublisher.publish(stepsMessage);
+
+      comMessage = getCenterOfMassTrajectoryMessage();
+      centerOfMassTrajectoryPublisher.publish(comMessage);
+
+      bodyHeightMessage = getBodyHeightMessage();
+      bodyHeightTrajectoryPublisher.publish((bodyHeightMessage));
+
+      conductor.addTerminalGoal(QuadrupedTestGoals.timeInFuture(variables, 10.0));
+      conductor.simulate();
+
+      // check robot is still upright and walked forward
+      Point3D expectedFinalPlanarPosition = getFinalPlanarPosition();
+      assertEquals(variables.getRobotBodyX().getDoubleValue(), expectedFinalPlanarPosition.getX(), 0.1);
+      assertEquals(variables.getRobotBodyY().getDoubleValue(), expectedFinalPlanarPosition.getY(), 0.1);
+      assertEquals(variables.getRobotBodyYaw().getDoubleValue(), expectedFinalPlanarPosition.getZ(), 0.1);
+      assertTrue(variables.getRobotBodyZ().getDoubleValue() > 0.0);
+      conductor.concludeTesting();
+   }
+
+   /**
+   * Listen to TOWR messages
+   * */
+   public abstract boolean listenToTowr();
+
 
    /**
     * Steps to execute, not expressed in absolute time
