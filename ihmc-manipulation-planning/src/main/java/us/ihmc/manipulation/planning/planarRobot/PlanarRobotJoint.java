261d17c62b69527c9e4399e81a47e6cf46c0152f
InhoLee <ilee@ihmc.us>
2019-01-10 15:05:40 -0600

    Defined robot joint.

---
+++ ihmc-manipulation-planning/src/main/java/us/ihmc/manipulation/planning/planarRobot/PlanarRobotJoint.java
@@ -0,0 +1,66 @@
+package us.ihmc.manipulation.planning.planarRobot;
+
+import us.ihmc.euclid.tuple2D.Point2D;
+
+public class PlanarRobotJoint
+{
+   private final PlanarRobotJoint parentJoint;
+
+   private final double linkLength;
+
+   private double jointAngle;
+   private double endTipAngle;
+
+   private final Point2D jointPoint = new Point2D();
+   private final Point2D endTipPoint = new Point2D();
+
+   public PlanarRobotJoint(Point2D basePoint, double linkLength, double initialAngle)
+   {
+      this.parentJoint = null;
+      this.linkLength = linkLength;
+      setJointAngle(initialAngle);
+      updateJoint();
+   }
+
+   public PlanarRobotJoint(PlanarRobotJoint parentJoint, double linkLength, double initialAngle)
+   {
+      this.parentJoint = parentJoint;
+      this.linkLength = linkLength;
+      setJointAngle(initialAngle);
+      updateJoint();
+   }
+
+   public void setJointAngle(double jointAngle)
+   {
+      // to set joint angle.
+      this.jointAngle = jointAngle;
+   }
+
+   public void updateJoint()
+   {
+      // to update end tip angle, joint point, end tip point.
+      if (parentJoint == null)
+      {
+         this.endTipAngle = jointAngle;
+         this.endTipPoint.set(jointPoint);
+         this.endTipPoint.add(linkLength * Math.cos(endTipAngle), linkLength * Math.sin(endTipAngle));
+      }
+      else
+      {
+         this.jointPoint.set(parentJoint.endTipPoint);
+         this.endTipAngle = parentJoint.endTipAngle + jointAngle;
+         this.endTipPoint.set(parentJoint.endTipPoint);
+         this.endTipPoint.add(linkLength * Math.cos(endTipAngle), linkLength * Math.sin(endTipAngle));
+      }
+   }
+
+   public Point2D getJointPoint()
+   {
+      return jointPoint;
+   }
+
+   public Point2D getEndTip()
+   {
+      return endTipPoint;
+   }
+}
