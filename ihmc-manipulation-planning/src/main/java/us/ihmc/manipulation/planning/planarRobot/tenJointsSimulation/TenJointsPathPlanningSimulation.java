db56553d14915d378280cc6b96afeeff615df80e
InhoLee <ilee@ihmc.us>
2019-01-14 11:40:34 -0600

    Planned path planning simualtion.

---
+++ ihmc-manipulation-planning/src/main/java/us/ihmc/manipulation/planning/planarRobot/tenJointsSimulation/TenJointsPathPlanningSimulation.java
@@ -0,0 +1,13 @@
+package us.ihmc.manipulation.planning.planarRobot.tenJointsSimulation;
+
+public class TenJointsPathPlanningSimulation
+{
+   
+   // TODO : plot in white.
+   // TODO : plotting window size.
+   
+   // rrt in blank space
+   // rrt in complex space with obstacles
+   // rrt in ultimately complex space
+   // rrt with additionally specified tasks
+}
