bd557cd4f88df05d823ca3949065de883f3f3cd6
thutcheson <thutcheson@ihmc-rl-rea.ihmc.us>
2019-02-11 08:36:26 -0600

    Added test for all COncaveHullTools except toString() methods.

--- robot-environment-awareness/src/test/java/us/ihmc/robotEnvironmentAwareness/geometry/ConcaveHullToolsTest.java
+++ robot-environment-awareness/src/test/java/us/ihmc/robotEnvironmentAwareness/geometry/ConcaveHullToolsTest.java
@@ -2,6 +2,9 @@ package us.ihmc.robotEnvironmentAwareness.geometry;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static us.ihmc.commons.lists.ListWrappingIndexTools.getNext;
+import static us.ihmc.commons.lists.ListWrappingIndexTools.getPrevious;
+import static us.ihmc.commons.lists.ListWrappingIndexTools.next;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +15,9 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import javax.vecmath.Point2d;
+import javax.vecmath.Vector2d;
 
 import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.junit.After;
@@ -35,12 +41,14 @@ import us.ihmc.euclid.geometry.interfaces.LineSegment2DReadOnly;
 import us.ihmc.euclid.geometry.interfaces.Vertex2DSupplier;
 import us.ihmc.euclid.geometry.interfaces.Vertex3DSupplier;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryRandomTools;
+import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.transform.RigidBodyTransform;
 import us.ihmc.euclid.tuple2D.Point2D;
 import us.ihmc.euclid.tuple2D.Vector2D;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DBasics;
 import us.ihmc.euclid.tuple2D.interfaces.Point2DReadOnly;
+import us.ihmc.euclid.tuple2D.interfaces.Vector2DReadOnly;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DBasics;
 import us.ihmc.javaFXToolkit.messager.JavaFXMessager;
@@ -263,13 +271,186 @@ public class ConcaveHullToolsTest
 			if( pocket != null)
 			{
 				assert(vertex==2);
-				System.out.printf("ConcaveHullToolsTest: pocket at vertex %d depth is %f", vertex, pocket.getMaxDepth());
+				assertEquals(pocket.getMaxDepth(), 0.429561, EPS);
+				//System.out.printf("ConcaveHullToolsTest: pocket at vertex %d depth is %f", vertex, pocket.getMaxDepth());
 			}
 			vertex++;
 		} while(pocket == null);
 		assertEquals(pocket.getMaxDepth(), 0.42956, EPS);		
 	}
 
+
+	@Test(timeout = 30000)
+	public void testEnsureCounterClockwiseOrdering() throws Exception
+	{
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		ConcaveHullTools.ensureCounterClockwiseOrdering(concaveHullVertices);
+		
+		//public static void ensureCounterClockwiseOrdering(List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.println("\ntestEnsureCounterClockwiseOrdering: ");
+	  }
+
+
+	@Test(timeout = 30000)
+	public void testComputePerimeter() throws Exception
+	{
+	List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+	double perimeter = ConcaveHullTools.computePerimeter(concaveHullVertices);
+	assertEquals(perimeter, 43.355019, EPS);
+	
+	//public static double computePerimeter(List<? extends Point2DReadOnly> concaveHullVertices)
+	//System.out.printf("\ntestComputePerimeter: %f", perimeter);
+	  }
+
+	@Test(timeout = 30000)
+	public void testRemoveSuccessiveDuplicateVertices() throws Exception
+	{
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		int successiveDuplicateVertices = ConcaveHullTools.removeSuccessiveDuplicateVertices(concaveHullVertices);
+		assert(successiveDuplicateVertices == 0);
+
+		//public static int removeSuccessiveDuplicateVertices(List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.printf("\ntestRemoveSuccessiveDuplicateVertices: %d", successiveDuplicateVertices);
+	}
+
+
+	   
+	@Test(timeout = 30000)
+	public void testComputeConcaveHullPocket() throws Exception
+	{
+		int concaveVertexIndex = 0;
+		ConcaveHullPocket pocketToPack = new ConcaveHullPocket();
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		boolean concaveHullPocket = ConcaveHullTools.computeConcaveHullPocket(concaveVertexIndex, pocketToPack, concaveHullVertices);
+		assert(concaveHullPocket == false);
+		
+		//public static boolean computeConcaveHullPocket(int concaveVertexIndex, ConcaveHullPocket pocketToPack, List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.printf("\ntestComputeConcaveHullPocket: %b", concaveHullPocket);
+   	}
+
+   	
+   @Test(timeout = 30000)
+	public void testFindConcaveHullPockets() throws Exception
+	{
+		double depthThreshold = 0;
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		Set<ConcaveHullPocket> concaveHullPockets = ConcaveHullTools.findConcaveHullPockets(concaveHullVertices, depthThreshold);	
+
+		assert(concaveHullPockets != null);
+		assert(concaveHullPockets.size() == 6) ;
+		
+		//public static Set<ConcaveHullPocket> findConcaveHullPockets(List<? extends Point2DReadOnly> concaveHullVertices, double depthThreshold)
+		//System.out.println("\ntestFindConcaveHullPockets: " + concaveHullPockets.size());
+	}
+	 
+		  
+	@Test(timeout = 30000)
+	public void testFindClosestIntersectionWithRay() throws Exception
+	{
+		Point2DReadOnly rayOrigin = new Point2D(0,0);
+		Vector2DReadOnly rayDirection = new Vector2D(0,0);
+		int startSearchIndex = 0, endSearchIndex = 0;
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		Point2DBasics intersectionToPack = (Point2DBasics) new Point2D(0,0);
+		
+		int closestIntersectionWithRay = ConcaveHullTools.findClosestIntersectionWithRay(rayOrigin, rayDirection, startSearchIndex, endSearchIndex, 
+		                                                                concaveHullVertices, intersectionToPack);
+		assert(closestIntersectionWithRay == -1);
+
+		//public static int findClosestIntersectionWithRay(Point2DReadOnly rayOrigin, Vector2DReadOnly rayDirection, int startSearchIndex, int endSearchIndex,
+		//List<? extends Point2DReadOnly> concaveHullVertices, Point2DBasics intersectionToPack)
+		//System.out.printf("\ntestFindClosestIntersectionWithRay: %d", closestIntersectionWithRay);
+	}
+	
+	
+	@Test(timeout = 30000)
+	public void testFirstConcaveHullPocketInefficient() throws Exception
+	{
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 	
+		ConcaveHullPocket firstConcaveHullPocketInefficient = ConcaveHullTools.findFirstConcaveHullPocketInefficient(concaveHullVertices);
+		assert(firstConcaveHullPocketInefficient != null);
+		if(firstConcaveHullPocketInefficient != null)
+		{
+			assert(firstConcaveHullPocketInefficient.getDeepestVertexIndex() == 18);
+			assertEquals(firstConcaveHullPocketInefficient.getMaxDepth(), 1.27138, EPS);
+		}
+		
+		//public static ConcaveHullPocket findFirstConcaveHullPocketInefficient(List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.println("\ntestFirstConcaveHullPocketInefficient: getDeepestVertexIndex() = " + firstConcaveHullPocketInefficient.getDeepestVertexIndex());
+		//System.out.println("\ntestFirstConcaveHullPocketInefficient: getMaxDepth() = " + firstConcaveHullPocketInefficient.getMaxDepth());
+	}
+	  	
+	
+	@Test(timeout = 30000)
+	public void testFindInnerClosestEdgeToVertex() throws Exception
+	{
+		int vertexIndex = 0, deadIndexRegion = 0;
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		Point2DBasics closestPointToPack = new Point2D(0,0);		
+		int innerClosestEdgeToVertex = ConcaveHullTools.findInnerClosestEdgeToVertex(vertexIndex, deadIndexRegion, 
+		                                                                           concaveHullVertices, closestPointToPack);
+		assert( innerClosestEdgeToVertex == 1);
+		
+		//public static int findInnerClosestEdgeToVertex(int vertexIndex, int deadIndexRegion, 
+		//List<? extends Point2DReadOnly> concaveHullVertices, Point2DBasics closestPointToPack)
+		//System.out.printf("\ntestFindInnerClosestEdgeToVertex: %d", innerClosestEdgeToVertex);
+	}
+	
+   
+	@Test(timeout = 30000)
+	public void testIsVertexPreventingKink() throws Exception
+	{
+		int vertexIndex = 0;
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D;
+		boolean isVertexPreventingKink =  ConcaveHullTools.isVertexPreventingKink(vertexIndex, concaveHullVertices);
+		assert(isVertexPreventingKink == true);
+		
+		//public static boolean isVertexPreventingKink(int vertexIndex, List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.printf("\ntestIsVertexPreventingKink: %b", isVertexPreventingKink);
+	}
+ 
+	
+	@Test(timeout = 30000)
+	public void testIsAlmostConvexAtVertex() throws Exception
+	{
+		int vertexIndex = 0;
+		double angleTolerance = 0;
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		boolean isAlmostConvexAtVertex = ConcaveHullTools.isAlmostConvexAtVertex(vertexIndex, angleTolerance, concaveHullVertices);
+		assert(isAlmostConvexAtVertex == true);
+		
+		//public static boolean isAlmostConvexAtVertex(int vertexIndex, double angleTolerance, List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.printf("\nisAlmostConvexAtVertex: %b", isAlmostConvexAtVertex);
+	}
+	
+	
+	@Test(timeout = 30000)
+	public void testGetAngleABC() throws Exception
+	{
+		Point2DReadOnly nextVertex = new Point2D(0,0);
+		Point2DReadOnly previousVertex = new Point2D(0,0);
+		Point2DReadOnly vertex = new Point2D(0,0);
+		double angleABC = ConcaveHullTools.getAngleABC(nextVertex, previousVertex, vertex);
+		assertEquals(angleABC, 0, EPS);
+		
+		//public static double getAngleABC(Point2DReadOnly nextVertex, Point2DReadOnly previousVertex, Point2DReadOnly vertex)
+		//System.out.printf("\ntestGetAngleABC: %f", angleABC);
+	}
+	
+	
+	@Test(timeout = 30000)
+	public void testGetAngleFromPreviousEdgeToNextEdge() throws Exception
+	{
+		int vertexIndex = 0;
+		List<? extends Point2DReadOnly> concaveHullVertices = pointcloud2D; 
+		double angleFromPreviousEdgeToNextEdge = ConcaveHullTools.getAngleFromPreviousEdgeToNextEdge(vertexIndex, concaveHullVertices);
+		assertEquals(angleFromPreviousEdgeToNextEdge, -2.189926, EPS);
+		
+		//public static double getAngleFromPreviousEdgeToNextEdge(int vertexIndex, List<? extends Point2DReadOnly> concaveHullVertices)
+		//System.out.printf("\ntestGetAngleFromPreviousEdgeToNextEdge: %f", angleFromPreviousEdgeToNextEdge);
+	}
+	
+	
 	@Before
 	public void setup() throws Exception
 	{
