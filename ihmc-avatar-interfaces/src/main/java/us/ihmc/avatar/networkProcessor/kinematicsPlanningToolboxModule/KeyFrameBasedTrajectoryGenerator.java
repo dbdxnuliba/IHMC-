12e6c9df4a9df8607109dfa65789927cc889fab1
InhoLee <ilee@ihmc.us>
2019-03-04 16:48:58 -0600

    Defined empty velocity optimizer.

--- ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsPlanningToolboxModule/KeyFrameBasedTrajectoryGenerator.java
+++ ihmc-avatar-interfaces/src/main/java/us/ihmc/avatar/networkProcessor/kinematicsPlanningToolboxModule/KeyFrameBasedTrajectoryGenerator.java
@@ -47,90 +47,11 @@ public class KeyFrameBasedTrajectoryGenerator
    public void compute(double searchingTimeTick)
    {
       initializeTrajectoryGenerator();
-      int numberOfTicks = (int) (keyFrameTimes.get(keyFrameTimes.size() - 1) / searchingTimeTick);
-      for (String jointName : jointNames)
-      {
-         double time = 0.0;
-         TDoubleArrayList velocities = new TDoubleArrayList();
-         for (int i = 0; i < numberOfTicks; i++)
-         {
-            int indexOfTrajectory = 0;
-            for (int j = keyFrameTimes.size() - 1; j > 0; j--)
-            {
-               if (keyFrameTimes.get(j) < time)
-               {
-                  indexOfTrajectory = j;
-                  break;
-               }
-            }
-            Trajectory trajectory = jointNameToTrajectoriesMap.get(jointName).get(indexOfTrajectory);
-            trajectory.compute(time);
-            velocities.add(trajectory.getVelocity());
-
-            time += searchingTimeTick;
-         }
-         Pair<Double, Double> minMaxVelocity = new Pair<Double, Double>(velocities.min(), velocities.max());
-         jointNameToVelocityBoundMap.put(jointName, minMaxVelocity);
-      }
+      computeOptimizedVelocity();
+      computeMaximumVelocity(searchingTimeTick);
 
       if (DEBUG_TRAJECTORY_PREVIEW)
-      {
-         FileWriter positionFW, velocityFW;
-         try
-         {
-            positionFW = new FileWriter(new File(this.getClass().getSimpleName() + "_position.csv"));
-            velocityFW = new FileWriter(new File(this.getClass().getSimpleName() + "_velocity.csv"));
-
-            positionFW.write(String.format("time"));
-            velocityFW.write(String.format("time"));
-            for (String jointName : jointNames)
-            {
-               positionFW.write(String.format("\t%s", jointName));
-               velocityFW.write(String.format("\t%s", jointName));
-            }
-            positionFW.write(System.lineSeparator());
-            velocityFW.write(System.lineSeparator());
-
-            double time = 0.0;
-            for (int i = 0; i < numberOfTicks; i++)
-            {
-               int indexOfTrajectory = 0;
-               for (int j = keyFrameTimes.size() - 1; j > 0; j--)
-               {
-                  if (keyFrameTimes.get(j) < time)
-                  {
-                     indexOfTrajectory = j;
-                     break;
-                  }
-               }
-
-               positionFW.write(String.format("%.4f (%d)", time, indexOfTrajectory));
-               velocityFW.write(String.format("%.4f (%d)", time, indexOfTrajectory));
-               for (String jointName : jointNames)
-               {
-                  Trajectory trajectory = jointNameToTrajectoriesMap.get(jointName).get(indexOfTrajectory);
-                  trajectory.compute(time);
-
-                  positionFW.write(String.format("\t%.4f", trajectory.getPosition()));
-                  velocityFW.write(String.format("\t%.4f", trajectory.getVelocity()));
-               }
-
-               positionFW.write(System.lineSeparator());
-               velocityFW.write(System.lineSeparator());
-               time += searchingTimeTick;
-            }
-
-            positionFW.close();
-            velocityFW.close();
-         }
-         catch (IOException ex)
-         {
-            ex.printStackTrace();
-         }
-
-         LogTools.info("done");
-      }
-
+         saveJointPositionAndVelocity(searchingTimeTick);
    }
 
    public void addInitialConfiguration(KinematicsToolboxOutputStatus initialConfiguration)
@@ -166,7 +87,6 @@ public class KeyFrameBasedTrajectoryGenerator
             converter.updateFullRobotModel(keyFrame);
             double point = converter.getFullRobotModel().getOneDoFJointByName(jointName).getQ();
             points.add(point);
-            LogTools.info("pos " + jointName + " " + point);
          }
 
          TDoubleArrayList velocities = new TDoubleArrayList();
@@ -176,7 +96,6 @@ public class KeyFrameBasedTrajectoryGenerator
             double diff = points.get(j + 1) - points.get(j);
             double timeDiff = keyFrameTimes.get(j + 1) - keyFrameTimes.get(j);
             velocities.add(diff / timeDiff);
-            LogTools.info("vel " + jointName + " " + (diff / timeDiff));
          }
          velocities.add(0.0);
 
@@ -185,12 +104,103 @@ public class KeyFrameBasedTrajectoryGenerator
          {
             Trajectory cubic = new Trajectory(4);
             cubic.setCubic(keyFrameTimes.get(j), keyFrameTimes.get(j + 1), points.get(j), velocities.get(j), points.get(j + 1), velocities.get(j + 1));
-            LogTools.info("cubic input " + keyFrameTimes.get(j) + " " + keyFrameTimes.get(j + 1) + " " + points.get(j) + " " + velocities.get(j) + " "
-                  + points.get(j + 1) + " " + velocities.get(j + 1));
             trajectories.add(cubic);
          }
          jointNameToTrajectoriesMap.put(jointName, trajectories);
       }
+   }
+
+   private void computeMaximumVelocity(double searchingTimeTick)
+   {
+      int numberOfTicks = (int) (keyFrameTimes.get(keyFrameTimes.size() - 1) / searchingTimeTick);
+      for (String jointName : jointNames)
+      {
+         double time = 0.0;
+         TDoubleArrayList velocities = new TDoubleArrayList();
+         for (int i = 0; i < numberOfTicks; i++)
+         {
+            int indexOfTrajectory = 0;
+            for (int j = keyFrameTimes.size() - 1; j > 0; j--)
+            {
+               if (keyFrameTimes.get(j) < time)
+               {
+                  indexOfTrajectory = j;
+                  break;
+               }
+            }
+            Trajectory trajectory = jointNameToTrajectoriesMap.get(jointName).get(indexOfTrajectory);
+            trajectory.compute(time);
+            velocities.add(trajectory.getVelocity());
+
+            time += searchingTimeTick;
+         }
+         Pair<Double, Double> minMaxVelocity = new Pair<Double, Double>(velocities.min(), velocities.max());
+         jointNameToVelocityBoundMap.put(jointName, minMaxVelocity);
+      }
+   }
+
+   private void saveJointPositionAndVelocity(double searchingTimeTick)
+   {
+      int numberOfTicks = (int) (keyFrameTimes.get(keyFrameTimes.size() - 1) / searchingTimeTick);
+      FileWriter positionFW, velocityFW;
+      try
+      {
+         positionFW = new FileWriter(new File(this.getClass().getSimpleName() + "_position.csv"));
+         velocityFW = new FileWriter(new File(this.getClass().getSimpleName() + "_velocity.csv"));
+
+         positionFW.write(String.format("time"));
+         velocityFW.write(String.format("time"));
+         for (String jointName : jointNames)
+         {
+            positionFW.write(String.format("\t%s", jointName));
+            velocityFW.write(String.format("\t%s", jointName));
+         }
+         positionFW.write(System.lineSeparator());
+         velocityFW.write(System.lineSeparator());
+
+         double time = 0.0;
+         for (int i = 0; i < numberOfTicks; i++)
+         {
+            int indexOfTrajectory = 0;
+            for (int j = keyFrameTimes.size() - 1; j > 0; j--)
+            {
+               if (keyFrameTimes.get(j) < time)
+               {
+                  indexOfTrajectory = j;
+                  break;
+               }
+            }
+
+            positionFW.write(String.format("%.4f (%d)", time, indexOfTrajectory));
+            velocityFW.write(String.format("%.4f (%d)", time, indexOfTrajectory));
+            for (String jointName : jointNames)
+            {
+               Trajectory trajectory = jointNameToTrajectoriesMap.get(jointName).get(indexOfTrajectory);
+               trajectory.compute(time);
+
+               positionFW.write(String.format("\t%.4f", trajectory.getPosition()));
+               velocityFW.write(String.format("\t%.4f", trajectory.getVelocity()));
+            }
+
+            positionFW.write(System.lineSeparator());
+            velocityFW.write(System.lineSeparator());
+            time += searchingTimeTick;
+         }
+
+         positionFW.close();
+         velocityFW.close();
+      }
+      catch (IOException ex)
+      {
+         ex.printStackTrace();
+      }
+
+      LogTools.info("done");
+   }
+
+   private void computeOptimizedVelocity()
+   {
+
    }
 
    public double getJointVelocityUpperBound(String jointName)
