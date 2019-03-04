2f49b028c9b3cc11a551dc8028270d084fc0eee5
thutcheson <thutcheson@ihmc-rl-rea.ihmc.us>
2019-03-04 11:54:54 -0600

    Commit.

--- robot-environment-awareness/src/test/java/us/ihmc/robotEnvironmentAwareness/geometry/PointMeanTest.java
+++ robot-environment-awareness/src/test/java/us/ihmc/robotEnvironmentAwareness/geometry/PointMeanTest.java
@@ -4,45 +4,45 @@ import static org.junit.Assert.*;
 
 import java.util.Random;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
+import us.ihmc.commons.MutationTestFacilitator;
+import us.ihmc.euclid.tools.EuclidCoreTestTools;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Tuple3DBasics;
 import us.ihmc.robotics.random.RandomGeometry;
 
-public class PointMeanTest
+public class PointMeanTest extends ConcaveHullTestBasics
 {
-	private static final int NUMBER_OF_ITERATIONS = 10000;
+	private static final int SEED = 0;
+	//private static final int ITERATIONS = 100;
 	private static final double EPS = 1.0e-12;
-	Random random = new Random();
 
-	@ContinuousIntegrationTest(estimatedDuration = 0.1)
-	@Test(timeout = 30000)
+	
+	@SuppressWarnings({"deprecation", "unused"})
+	@Test
 	public final void testVectorMean()
 	{
-		Point3D p = new Point3D();
 		Point3D sum = new Point3D();
 		PointMean pMean = new PointMean();
+		Random random = new Random();
 
-		for (int i = 0; i < NUMBER_OF_ITERATIONS; i++)
-		{
-			p = RandomGeometry.nextPoint3D(random, 0, 1);
-			pMean.update(p);
-			sum.add(p);
-		}
-
-		sum.scale(1.0 / NUMBER_OF_ITERATIONS);
-
+		random.setSeed(SEED);
+		for (int i=0; i < ITERATIONS; i++)
+			sum.add(RandomGeometry.nextPoint3D(random, 0, 1));			
+		sum.scale(1.0 / ITERATIONS);
+		
+		random.setSeed(0);
+		for (int i=0; i < ITERATIONS; i++)
+			pMean.update(RandomGeometry.nextPoint3D(random, 0, 1));		
+		
+		EuclidCoreTestTools.assertTuple3DEquals(pMean, sum, EPS);
 		assertEquals(pMean.getX(), sum.getX(), EPS);
 		assertEquals(pMean.getY(), sum.getY(), EPS);
 		assertEquals(pMean.getZ(), sum.getZ(), EPS);
+		assertEquals(pMean.getNumberOfSamples(), ITERATIONS);
 
 		pMean.clear();
 		assertEquals(pMean.getX(), 0, EPS);
@@ -50,27 +50,26 @@ public class PointMeanTest
 		assertEquals(pMean.getZ(), 0, EPS);
 	}
 
-	@ContinuousIntegrationTest(estimatedDuration = 0.1)
-	@Test(timeout = 30000)
+	@Test
 	public final void testWithTuple3DBasics()
 	{
-		Point3D p = new Point3D();
 		Point3D sum = new Point3D();
 		PointMean pMean = new PointMean();
+		Random random = new Random();
 
-		for (int i = 0; i < NUMBER_OF_ITERATIONS; i++)
-		{
-			p = RandomGeometry.nextPoint3D(random, 0, 1);
-			pMean.update((Tuple3DBasics) p, 1);
-			sum.add(p);
-		}
+		random.setSeed(SEED);
+		for (int i = 0; i < ITERATIONS; i++)
+			sum.add(RandomGeometry.nextPoint3D(random, 0, 1));
+		sum.scale(1.0 / ITERATIONS);
 
-		sum.scale(1.0 / NUMBER_OF_ITERATIONS);
-
+		random.setSeed(0);
+		for (int i = 0; i < ITERATIONS; i++)
+			pMean.update((Tuple3DBasics) RandomGeometry.nextPoint3D(random, 0, 1), 1);
+		
 		assertEquals(pMean.getX(), sum.getX(), EPS);
 		assertEquals(pMean.getY(), sum.getY(), EPS);
 		assertEquals(pMean.getZ(), sum.getZ(), EPS);
-		assertEquals(pMean.getNumberOfSamples(), NUMBER_OF_ITERATIONS);
+		assertEquals(pMean.getNumberOfSamples(), ITERATIONS);
 
 		pMean.clear();
 		assertEquals(pMean.getX(), 0, EPS);
@@ -78,4 +77,9 @@ public class PointMeanTest
 		assertEquals(pMean.getZ(), 0, EPS);
 	}
 
+	public static void main(String[] args)
+	{
+		MutationTestFacilitator.facilitateMutationTestForClass(PointMeanTest.class, PointMeanTest.class);
+	}
+		
 }
