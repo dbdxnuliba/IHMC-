79843d77b8152901ef96c87e6fbf479bfe8269ea
thutcheson <thutcheson@ihmc-rl-rea.ihmc.us>
2019-03-04 12:02:20 -0600

    Updated for JUnit5

--- robot-environment-awareness/src/test/java/us/ihmc/robotEnvironmentAwareness/geometry/ColorValueToRGBConversionTest.java
+++ robot-environment-awareness/src/test/java/us/ihmc/robotEnvironmentAwareness/geometry/ColorValueToRGBConversionTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Disabled;
+
+import us.ihmc.commons.MutationTestFacilitator;
 import us.ihmc.robotEnvironmentAwareness.ui.graphicsBuilders.StereoVisionPointCloudViewer;
 
-public class ColorValueToRGBConversionTest
+public class ColorValueToRGBConversionTest extends ConcaveHullTestBasics
 {
-   private static final int NUMBER_OF_TESTS = 100;
    private static final double EPS = 1.0e-4;
    private static final double rgbScale = 255.0;
 
@@ -21,8 +22,8 @@ public class ColorValueToRGBConversionTest
    public void testRandomColor()
    {
       Random random = new Random(0612L);
-
-      for (int i = 0; i < NUMBER_OF_TESTS; i++)
+      int nTests = 0;
+      for (int i = 0; i < ITERATIONS; i++)
       {
          int randomColorValue = random.nextInt();
          Color randomColor = new Color(randomColorValue);
@@ -31,6 +32,14 @@ public class ColorValueToRGBConversionTest
          assertTrue("Red color conversion is working incorrect.", Math.abs(convertedRandomColor.getRed() * rgbScale - randomColor.getRed()) < EPS);
          assertTrue("Green color conversion is working incorrect.", Math.abs(convertedRandomColor.getGreen() * rgbScale - randomColor.getGreen()) < EPS);
          assertTrue("Blue color conversion is working incorrect.", Math.abs(convertedRandomColor.getBlue() * rgbScale - randomColor.getBlue()) < EPS);
+         nTests++;
       }
+      assert(nTests == ITERATIONS);
    }
+   
+	public static void main(String[] args)
+	{
+		MutationTestFacilitator.facilitateMutationTestForClass(ColorValueToRGBConversionTest.class, ColorValueToRGBConversionTest.class);
+	}
+
 }
