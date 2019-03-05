77ef871e5b76515ddddefb67dd495c86a9cf8652
InhoLee <ilee@ihmc.us>
2019-03-05 11:13:45 -0600

    Several types of optimization are tested.

--- ihmc-manipulation-planning/src/main/java/KeyFrameBasedTrajectoryVelocityOptimizerTest.java
+++ ihmc-manipulation-planning/src/main/java/KeyFrameBasedTrajectoryVelocityOptimizerTest.java
@@ -16,6 +16,11 @@ public class KeyFrameBasedTrajectoryVelocityOptimizerTest
    private final TDoubleArrayList times = new TDoubleArrayList();
    private static final double velocitOptimizerDT = 0.001;
 
+   private enum OptimizationType
+   {
+      Velocity, Acceleration, Jerk, KineticEnergy
+   }
+
    public KeyFrameBasedTrajectoryVelocityOptimizerTest()
    {
       defineTrajectoryPoints();
@@ -25,49 +30,31 @@ public class KeyFrameBasedTrajectoryVelocityOptimizerTest
       initial.addAll(velocities);
 
       for (int i = 0; i < initial.size(); i++)
-         System.out.println("initial is " + initial.get(i));
+         System.out.println("# initial input is " + initial.get(i));
 
-      SingleQueryFunction function = new SingleQueryFunction()
-      {
-         @Override
-         public double getQuery(TDoubleArrayList values)
-         {
-            int numberOfTicks = (int) (times.get(times.size() - 1) / velocitOptimizerDT);
-            double time = 0.0;
-            double kineticEnergy = 0.0;
-            for (int i = 0; i < numberOfTicks; i++)
-            {
-               int indexOfTrajectory = findTrajectoryIndex(time);
-               TDoubleArrayList velocities = new TDoubleArrayList();
-               velocities.add(0.0);
-               velocities.addAll(values);
-               velocities.add(0.0);
-               List<Trajectory> trajectories = calculateTrajectories(times, positions, velocities);
-               Trajectory trajectory = trajectories.get(indexOfTrajectory);
-               trajectory.compute(time);
-
-               // TODO : compare which way is better between square of velocity and absolute value of velocity.
-               //kineticEnergy += trajectory.getVelocity() * trajectory.getVelocity() * velocitOptimizerDT;
-               //kineticEnergy += Math.abs(trajectory.getVelocity()) * velocitOptimizerDT;
-               kineticEnergy += Math.abs(trajectory.getAcceleration()) * velocitOptimizerDT;
-               time += velocitOptimizerDT;
-            }
-            return kineticEnergy;
-         }
-      };
-      GradientDescentModule solver = new GradientDescentModule(function, initial);
-      solver.setMaximumIterations(100);
-
-      LogTools.info("initial energy " + function.getQuery(initial));
       TDoubleArrayList initialVelocities = new TDoubleArrayList();
       initialVelocities.add(0.0);
       initialVelocities.addAll(initial);
       initialVelocities.add(0.0);
       List<Trajectory> originalTrajectories = calculateTrajectories(times, positions, initialVelocities);
-      saveJointPositionAndVelocity("initial", originalTrajectories, velocitOptimizerDT);
+      saveJointPositionAndVelocity("initialTrajectory", originalTrajectories, velocitOptimizerDT);
+      
+      runOptimizer(OptimizationType.Velocity, initial);
+      runOptimizer(OptimizationType.Acceleration, initial);
+      runOptimizer(OptimizationType.Jerk, initial);
+      runOptimizer(OptimizationType.KineticEnergy, initial);
+   }
 
-      System.out.println("iteration is " + solver.run());
-      TDoubleArrayList optimalSolution = solver.getOptimalInput();
+   private void runOptimizer(OptimizationType optimizationType, TDoubleArrayList initial)
+   {
+      SingleQueryFunction function = new EvaluationFunction(optimizationType);
+      GradientDescentModule optimizer = new GradientDescentModule(function, initial);
+      optimizer.setMaximumIterations(100);
+      
+      LogTools.info("initial " + optimizationType.toString() + function.getQuery(initial));
+
+      System.out.println("iteration is " + optimizer.run());
+      TDoubleArrayList optimalSolution = optimizer.getOptimalInput();
       for (int i = 0; i < optimalSolution.size(); i++)
          System.out.println("solution is " + optimalSolution.get(i));
 
@@ -76,18 +63,70 @@ public class KeyFrameBasedTrajectoryVelocityOptimizerTest
       finalVelocities.addAll(optimalSolution);
       finalVelocities.add(0.0);
       List<Trajectory> optimalTrajectories = calculateTrajectories(times, positions, finalVelocities);
-      saveJointPositionAndVelocity("optimal", optimalTrajectories, velocitOptimizerDT);
+      saveJointPositionAndVelocity("optimalTrajectory_" + optimizationType.toString(), optimalTrajectories, velocitOptimizerDT);
 
-      System.out.println("optimal query is " + solver.getOptimalQuery());
+      System.out.println("optimal " + optimizationType.toString() + optimizer.getOptimalQuery());
+   }
+
+   private class EvaluationFunction implements SingleQueryFunction
+   {
+      private OptimizationType optimizationType;
+
+      private EvaluationFunction(OptimizationType optimizationType)
+      {
+         this.optimizationType = optimizationType;
+      }
+
+      @Override
+      public double getQuery(TDoubleArrayList values)
+      {
+         int numberOfTicks = (int) (times.get(times.size() - 1) / velocitOptimizerDT);
+         double time = 0.0;
+         double kineticEnergy = 0.0;
+         for (int i = 1; i < numberOfTicks; i++)
+         {
+            int indexOfTrajectory = findTrajectoryIndex(time);
+            TDoubleArrayList velocities = new TDoubleArrayList();
+            velocities.add(0.0);
+            velocities.addAll(values);
+            velocities.add(0.0);
+            List<Trajectory> trajectories = calculateTrajectories(times, positions, velocities);
+            Trajectory trajectory = trajectories.get(indexOfTrajectory);
+            trajectory.compute(time);
+
+            double previousTime = time - velocitOptimizerDT;
+            int indexOfTrajectoryForPreviousTick = findTrajectoryIndex(previousTime);
+            Trajectory trajectoryForPreviousTick = trajectories.get(indexOfTrajectoryForPreviousTick);
+            trajectoryForPreviousTick.compute(previousTime);
+
+            switch (optimizationType)
+            {
+            case Velocity:
+               kineticEnergy += Math.abs(trajectory.getVelocity()) * velocitOptimizerDT;
+               break;
+            case Acceleration:
+               kineticEnergy += Math.abs(trajectory.getAcceleration()) * velocitOptimizerDT;
+               break;
+            case Jerk:
+               kineticEnergy += Math.abs(trajectory.getAcceleration() - trajectoryForPreviousTick.getAcceleration()) / velocitOptimizerDT;
+               break;
+            case KineticEnergy:
+               kineticEnergy += trajectory.getVelocity() * trajectory.getVelocity() * velocitOptimizerDT;
+               break;
+            }
+            time += velocitOptimizerDT;
+         }
+         return kineticEnergy;
+      }
    }
 
    private void defineTrajectoryPoints()
    {
       positions.add(1.0);
       positions.add(2.0);
-      positions.add(3.0);
       positions.add(4.0);
-      positions.add(5.0);
+      positions.add(3.0);
+      positions.add(1.0);
 
       times.add(0.0);
       times.add(1.0);
@@ -133,20 +172,13 @@ public class KeyFrameBasedTrajectoryVelocityOptimizerTest
       return velocities;
    }
 
-   public static void main(String[] args)
-   {
-      KeyFrameBasedTrajectoryVelocityOptimizerTest test = new KeyFrameBasedTrajectoryVelocityOptimizerTest();
-
-      LogTools.info("done");
-   }
-
    private void saveJointPositionAndVelocity(String namePrefix, List<Trajectory> trajectories, double timeTick)
    {
       int numberOfTicks = (int) (times.get(times.size() - 1) / timeTick);
       FileWriter positionFW;
       try
       {
-         positionFW = new FileWriter(new File(namePrefix + "_position_velocity.csv"));
+         positionFW = new FileWriter(new File(namePrefix + "_Pos_Vel_Acc.csv"));
 
          positionFW.write(String.format("time\t_position\t_velocity\t_acceleration"));
          positionFW.write(System.lineSeparator());
@@ -172,6 +204,13 @@ public class KeyFrameBasedTrajectoryVelocityOptimizerTest
       {
          ex.printStackTrace();
       }
+
+      LogTools.info("done");
+   }
+
+   public static void main(String[] args)
+   {
+      KeyFrameBasedTrajectoryVelocityOptimizerTest test = new KeyFrameBasedTrajectoryVelocityOptimizerTest();
 
       LogTools.info("done");
    }
