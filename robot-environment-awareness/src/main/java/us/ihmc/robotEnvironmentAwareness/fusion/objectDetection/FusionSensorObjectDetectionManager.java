25a623391249adbcfce7bc26d5ddaa33ccf1e7ff
inhol <ilee@ihmc.us>
2019-04-19 14:24:23 -0500

    Added point cloud data updater.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/objectDetection/FusionSensorObjectDetectionManager.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/objectDetection/FusionSensorObjectDetectionManager.java
@@ -2,6 +2,7 @@ package us.ihmc.robotEnvironmentAwareness.fusion.objectDetection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import controller_msgs.msg.dds.DoorParameterPacket;
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
@@ -14,25 +15,32 @@ public class FusionSensorObjectDetectionManager
    private final Ros2Node ros2Node;
    private final Map<ObjectType, AbstractObjectParameterCalculator<?>> objectTypeToCalculatorMap = new HashMap<>();
 
+   private final AtomicReference<StereoVisionPointCloudMessage> latestStereoVisionPointCloudMessage = new AtomicReference<>(null);
+
    public FusionSensorObjectDetectionManager(Ros2Node ros2Node)
    {
       this.ros2Node = ros2Node;
       defineCalculatorMap();
    }
-   
+
    private void defineCalculatorMap()
    {
       objectTypeToCalculatorMap.put(ObjectType.Door, new DoorParameterCalculator(ros2Node, DoorParameterPacket.class));
    }
    
+   public void updateLatestStereoVisionPointCloudMessage(StereoVisionPointCloudMessage message)
+   {
+      latestStereoVisionPointCloudMessage.set(message);
+   }
+
    public void computeAndPublish(ObjectType objectType, StereoVisionPointCloudMessage sourceMessage, RegionOfInterest roi)
    {
       long startTime = System.nanoTime();
       objectTypeToCalculatorMap.get(objectType).getPointCloudInROI(sourceMessage, roi);
       objectTypeToCalculatorMap.get(objectType).calculateAndPackResult();
       objectTypeToCalculatorMap.get(objectType).publish();
-      
+
       long computingTime = System.nanoTime() - startTime;
-      System.out.println("computing time is "+Conversions.nanosecondsToMicroseconds(computingTime));
+      System.out.println("computing time is " + Conversions.nanosecondsToMicroseconds(computingTime));
    }
 }
