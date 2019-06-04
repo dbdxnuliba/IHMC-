f025473ba585623d0d11a5b05ea7ec58f62c069f
Llama OCU <llama@ihmc>
2019-06-04 13:42:52 -0500

    Added paws graphics.

--- ihmc-quadruped-ui/src/main/java/us/ihmc/quadrupedUI/graphics/ManualStepPlanGraphic.java
+++ ihmc-quadruped-ui/src/main/java/us/ihmc/quadrupedUI/graphics/ManualStepPlanGraphic.java
@@ -2,28 +2,37 @@ package us.ihmc.quadrupedUI.graphics;
 
 import controller_msgs.msg.dds.QuadrupedTimedStepListMessage;
 import controller_msgs.msg.dds.QuadrupedTimedStepMessage;
+import jassimp.Jassimp;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Mesh;
 import javafx.scene.shape.MeshView;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Translate;
 import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.referenceFrame.FramePoint3D;
+import us.ihmc.javaFXToolkit.JavaFXTools;
+import us.ihmc.javaFXToolkit.graphics.JAssImpJavaFXTools;
 import us.ihmc.javaFXToolkit.shapes.JavaFXMultiColorMeshBuilder;
 import us.ihmc.javaFXToolkit.shapes.TextureColorAdaptivePalette;
 import us.ihmc.javaFXVisualizers.PrivateAnimationTimer;
 import us.ihmc.log.LogTools;
-import us.ihmc.quadrupedUI.QuadrupedUserInterface;
 import us.ihmc.robotics.robotSide.QuadrantDependentList;
 import us.ihmc.robotics.robotSide.RobotQuadrant;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ManualStepPlanGraphic extends Group
 {
    private static final double RADIUS = 0.02;
    private static final double zOffset = 0.01;
+   private static final QuadrantDependentList<Color> solutionFootstepColors = new QuadrantDependentList<>(Color.BLUE, Color.ORANGE, Color.DARKBLUE, Color.DARKORANGE);
 
    private final MeshView meshView = new MeshView();
    private final PrivateAnimationTimer animationTimer = new PrivateAnimationTimer(this::handle);
@@ -33,11 +42,23 @@ public class ManualStepPlanGraphic extends Group
    private Mesh mesh;
    private Material material;
 
+   private final Group pawGroup = new Group();
+   private final Mesh pawMesh;
+   private final AtomicReference<MeshView[]> paws = new AtomicReference<>(null);
+
    public ManualStepPlanGraphic()
    {
-      getChildren().addAll(meshView);
+      getChildren().addAll(pawGroup);
 
       animationTimer.start();
+      try
+      {
+         pawMesh = JAssImpJavaFXTools.getJavaFxMeshes("us/ihmc/quadrupedUI/model/paw.stl")[0].getMesh();
+      }
+      catch (URISyntaxException | IOException e)
+      {
+         throw new RuntimeException(e);
+      }
    }
 
    /**
@@ -55,9 +76,10 @@ public class ManualStepPlanGraphic extends Group
    {
       meshBuilder.clear();
 
-      QuadrantDependentList<Color> colors = QuadrupedUserInterface.feetColors;
+      QuadrantDependentList<Color> colors = solutionFootstepColors;
 
       FramePoint3D footPosition = new FramePoint3D();
+      MeshView[] newPaws = new MeshView[message.getQuadrupedStepList().size()];
 
       for (int i = 0; i < message.getQuadrupedStepList().size(); i++)
       {
@@ -68,7 +90,15 @@ public class ManualStepPlanGraphic extends Group
          footPosition.addZ(zOffset);
 
          meshBuilder.addSphere(RADIUS, footPosition, regionColor);
+
+         MeshView pawView = new MeshView(pawMesh);
+         pawView.getTransforms().add(new Translate(footPosition.getX(), footPosition.getY(), footPosition.getZ()));
+         pawView.getTransforms().add(new Scale(0.002, 0.002, 0.002));
+         pawView.setMaterial(new PhongMaterial(regionColor));
+         newPaws[i] = pawView;
       }
+
+      paws.set(newPaws);
 
       generateAndQueueForJavaFXUpdate();
    }
@@ -93,6 +123,12 @@ public class ManualStepPlanGraphic extends Group
 
    private void handle(long now)
    {
+      MeshView[] newPaws = paws.getAndSet(null);
+      if (newPaws != null)
+      {
+         pawGroup.getChildren().clear();
+         pawGroup.getChildren().addAll(newPaws);
+      }
       synchronized (this)
       {
          meshView.setMesh(mesh);
