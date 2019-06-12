6040739dca7aa180a10db0d692bb88b3df2300bf
Robert <rgriffin@ihmc.us>
2019-06-12 13:30:34 -0500

    theoretically got the simulation working

--- ihmc-quadruped-robotics/src/test/java/us/ihmc/quadrupedRobotics/controller/force/QuadrupedSwingOverPlanarRegionsTest.java
+++ ihmc-quadruped-robotics/src/test/java/us/ihmc/quadrupedRobotics/controller/force/QuadrupedSwingOverPlanarRegionsTest.java
@@ -23,6 +23,7 @@ import us.ihmc.quadrupedCommunication.teleop.RemoteQuadrupedTeleopManager;
 import us.ihmc.quadrupedPlanning.QuadrupedSpeed;
 import us.ihmc.quadrupedRobotics.QuadrupedForceTestYoVariables;
 import us.ihmc.quadrupedRobotics.QuadrupedMultiRobotTestInterface;
+import us.ihmc.quadrupedRobotics.QuadrupedTestBehaviors;
 import us.ihmc.quadrupedRobotics.QuadrupedTestFactory;
 import us.ihmc.quadrupedRobotics.controller.QuadrupedControlMode;
 import us.ihmc.quadrupedRobotics.model.QuadrupedInitialOffsetAndYaw;
@@ -52,12 +53,11 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
    private QuadrupedTestFactory quadrupedTestFactory;
 
 
-   private static final boolean LOCAL_MODE = true;
+   private static final boolean LOCAL_MODE = false;
 
    @Test
    public void testSwingOverPlanarRegions() throws SimulationExceededMaximumTimeException
    {
-
       double stepDuration = 0.33;
       double dwellDuration = 0.2;
       double stepLength = 0.3;
@@ -87,14 +87,18 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
       conductor.getScs().getRootRegistry().addChild(registry);
       conductor.getScs().addYoGraphicsListRegistry(yoGraphicsListRegistry);
 
-//      drcSimulationTestHelper.simulateAndBlockAndCatchExceptions(0.5);
+      QuadrupedTestBehaviors.standUp(conductor, variables);
+      QuadrupedTestBehaviors.startBalancing(conductor, variables, stepTeleopManager);
+
 
       FramePoint3D stanceFootPosition = new FramePoint3D();
       QuadrantDependentList<FramePoint3D> footPositions = new QuadrantDependentList<>();
       for (RobotQuadrant robotQuadrant : RobotQuadrant.values)
       {
-         footPositions.get(robotQuadrant).setToZero(quadrupedTestFactory.getFullRobotModel().getSoleFrame(robotQuadrant));
-         footPositions.get(robotQuadrant).changeFrame(ReferenceFrame.getWorldFrame());
+         FramePoint3D footPosition = new FramePoint3D();
+         footPosition.setToZero(quadrupedTestFactory.getFullRobotModel().getSoleFrame(robotQuadrant));
+         footPosition.changeFrame(ReferenceFrame.getWorldFrame());
+         footPositions.put(robotQuadrant, footPosition);
       }
 
       FramePoint3D swingStartPosition = new FramePoint3D();
@@ -104,6 +108,8 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
       swingEndPosition.set(0.0, stepWidth, 0.0);
 
       QuadrupedTimedStepListMessage stepListMessage = new QuadrupedTimedStepListMessage();
+      stepListMessage.setIsExpressedInAbsoluteTime(false);
+
       stepTeleopManager.getXGaitSettings().setEndPhaseShift(180.0);
       stepTeleopManager.getXGaitSettings().setQuadrupedSpeed(QuadrupedSpeed.MEDIUM);
       stepTeleopManager.getXGaitSettings().getTrotMediumTimings().setStepDuration(0.33);
@@ -151,9 +157,9 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
 
          frontMessage.getQuadrupedStepMessage().getGoalPosition().set(frontLocation);
          frontMessage.getQuadrupedStepMessage().setRobotQuadrant(frontQuadrant.toByte());
-         footstepData.setTrajectoryType(TrajectoryType.CUSTOM.toByte());
+         frontMessage.getQuadrupedStepMessage().setTrajectoryType(TrajectoryType.CUSTOM.toByte());
          for (int j = 0; j < swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().size(); j++)
-            footstepData.getCustomPositionWaypoints().add().set(swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().get(j));
+            frontMessage.getQuadrupedStepMessage().getCustomPositionWaypoints().add().set(swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().get(j));
 
          swingStartPosition.set(footPositions.get(hindQuadrant));
          swingEndPosition.set(hindLocation);
@@ -175,12 +181,12 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
 
          hindMessage.getQuadrupedStepMessage().getGoalPosition().set(hindLocation);
          hindMessage.getQuadrupedStepMessage().setRobotQuadrant(hindQuadrant.toByte());
-         footstepData.setTrajectoryType(TrajectoryType.CUSTOM.toByte());
+         hindMessage.getQuadrupedStepMessage().setTrajectoryType(TrajectoryType.CUSTOM.toByte());
          for (int j = 0; j < swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().size(); j++)
-            footstepData.getCustomPositionWaypoints().add().set(swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().get(j));
+            hindMessage.getQuadrupedStepMessage().getCustomPositionWaypoints().add().set(swingOverPlanarRegionsTrajectoryExpander.getExpandedWaypoints().get(j));
 
 
-         double maxSpeed = maxSpeedDimensionless / swingTime;
+         double maxSpeed = maxSpeedDimensionless / stepDuration;
          if (maxSpeed > maxSwingSpeed)
          {
             double adjustedSwingTime = maxSpeedDimensionless / maxSwingSpeed;
@@ -192,17 +198,19 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
             simulationTime += adjustedSwingTime;
          }
          else
-            simulationTime += swingTime;
+            simulationTime += stepDuration;
 
-         stepListMessage.getQuadrupedStepList().add(frontMessage);
-         stepListMessage.getQuadrupedStepList().add(hindMessage);
+         stepListMessage.getQuadrupedStepList().add().set(frontMessage);
+         stepListMessage.getQuadrupedStepList().add().set(hindMessage);
 
          footPositions.get(frontQuadrant).set(frontLocation);
          footPositions.get(hindQuadrant).set(hindLocation);
+
+         currentTime += stepDuration + dwellDuration;
       }
 
       stepTeleopManager.publishTimedStepListToController(stepListMessage);
-      conductor.addTimeLimit(variables.getYoTime(), simulationTime);
+      conductor.addTimeLimit(variables.getYoTime(), variables.getYoTime().getDoubleValue() + simulationTime);
       conductor.simulate();
 
       Point3D rootJointPosition = new Point3D(2.81, 0.0, 0.83);
@@ -234,7 +242,7 @@ public abstract class QuadrupedSwingOverPlanarRegionsTest implements QuadrupedMu
          LittleWallsWithIncreasingHeightPlanarRegionEnvironment environment = new LittleWallsWithIncreasingHeightPlanarRegionEnvironment();
 
          quadrupedTestFactory.setGroundProfile3D(environment.getTerrainObject3D());
-         quadrupedTestFactory.setInitialOffset(new QuadrupedInitialOffsetAndYaw(-0.5, 0.0, 0.0, Math.PI));
+         quadrupedTestFactory.setInitialOffset(new QuadrupedInitialOffsetAndYaw(-0.5, 0.0, 0.0, 0.0));
          quadrupedTestFactory.setControlMode(QuadrupedControlMode.FORCE);
          quadrupedTestFactory.setGroundContactModelType(QuadrupedGroundContactModelType.FLAT);
          quadrupedTestFactory.setUseNetworking(true);
