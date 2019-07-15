d8973aa7bd0451e7f4ddcc8750401d89437d4e8f
Stefano <stefano.dafarra@gmail.com>
2019-07-15 16:09:37 -0500

    Added script for generating the cpp interface to controller messages.
    The PauseWalkingMessage was having a whitespace before bool causing errors during the generation. The CMakeLists automatically include every newly created message.

---
+++ ihmc-interfaces/generate_messages_cpp.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "$SCRIPT_DIR"
+
+
+if [ -z "$ROS2_SETUP_PATH" ]
+then
+    if [ -z "$ROS_DISTRO" ]
+    then 
+      echo "\$ROS2_SETUP_PATH and \ROS_DISTRO are empty. Is ROS installed? If so, define the variable \$ROS2_SETUP_PATH pointing to the setup.sh script."
+      exit 1
+    fi
+
+    echo "Sourcing /opt/ros/$ROS_DISTRO/setup.bash"
+    source /opt/ros/$ROS_DISTRO/setup.bash 
+    
+else
+    echo "Sourcing $ROS2_SETUP_PATH"
+    source $ROS2_SETUP_PATH 
+fi
+
+touch $SCRIPT_DIR/bin/COLCON_IGNORE
+
+# Touching the CMakeLists file to be sure we are considering all the new messages
+touch $SCRIPT_DIR/src/main/messages/ihmc_interfaces/controller_msgs/CMakeLists.txt
+
+cd $SCRIPT_DIR
+
+echo "Generating C++ interfaces. This may take a while."
+colcon build --symlink-install
+
+retVal=$?
+if [ $retVal -eq 0 ]; then
+    echo "[HINT] Remember to source the file $CURRENT_DIR/install/setup.bash"
+fi
+
