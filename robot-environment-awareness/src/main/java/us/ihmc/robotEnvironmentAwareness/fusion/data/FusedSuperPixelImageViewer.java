6f6925bb82d04c3bc6a668dc9dc853862ef22bcd
Robert <rgriffin@ihmc.us>
2019-07-31 12:06:15 -0500

    pushed the viewing of the regions into an executor service to it doesnt lock up the process

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/data/FusedSuperPixelImageViewer.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/fusion/data/FusedSuperPixelImageViewer.java
@@ -3,6 +3,8 @@ package us.ihmc.robotEnvironmentAwareness.fusion.data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicReference;
 
 import javafx.collections.ObservableList;
@@ -10,8 +12,8 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.MeshView;
+import us.ihmc.commons.thread.ThreadTools;
 import us.ihmc.euclid.tuple3D.Point3D;
-import us.ihmc.euclid.tuple3D.Vector3D;
 import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Vector3DReadOnly;
 import us.ihmc.javaFXToolkit.messager.SharedMemoryJavaFXMessager;
@@ -22,30 +24,37 @@ import us.ihmc.robotEnvironmentAwareness.planarRegion.PlanarRegionSegmentationRa
 
 public class FusedSuperPixelImageViewer
 {
-   private final AtomicReference<RawSuperPixelImage> lidarImageFusionDataToRender;
-
    protected final JavaFXMultiColorMeshBuilder meshBuilder;
+
+   private final ExecutorService executorService = Executors.newSingleThreadExecutor(ThreadTools.getNamedThreadFactory(getClass().getSimpleName()));
+
 
    private final AtomicReference<MeshView> meshToRender = new AtomicReference<>(null);
    private final Group root = new Group();
    protected final ObservableList<Node> children = root.getChildren();
-   private final AtomicReference<Boolean> clear = new AtomicReference<>(false);
+   private final AtomicReference<Boolean> showSolution = new AtomicReference<>(true);
+   private final AtomicReference<Boolean> clearSolution = new AtomicReference<>(false);
 
    public FusedSuperPixelImageViewer(SharedMemoryJavaFXMessager messager)
    {
-      lidarImageFusionDataToRender = messager.createInput(LidarImageFusionAPI.FusionDataState, null);
-
       meshBuilder = new JavaFXMultiColorMeshBuilder(new TextureColorAdaptivePalette(2048));
 
-      messager.registerTopicListener(LidarImageFusionAPI.ShowFusionData, (content) -> unpackFusionData());
+      messager.registerTopicListener(LidarImageFusionAPI.FusionDataState, superPixelImage -> executorService.submit(() -> unpackFusionData(superPixelImage)));
+      messager.registerTopicListener(LidarImageFusionAPI.ShowFusionData, this::handleShowSolution);
    }
 
-   private void unpackFusionData()
+   private void handleShowSolution(boolean show)
+   {
+      showSolution.set(show);
+      if (!show)
+         clearSolution.set(true);
+   }
+
+   private void unpackFusionData(RawSuperPixelImage rawSuperPixelImage)
    {
       clear();
       double lineWidth = 0.01;
       meshBuilder.clear();
-      RawSuperPixelImage rawSuperPixelImage = lidarImageFusionDataToRender.get();
 
       if (rawSuperPixelImage == null)
          return;
@@ -95,10 +104,10 @@ public class FusedSuperPixelImageViewer
    {
       MeshView newScanMeshView = meshToRender.getAndSet(null);
 
-      if (clear.getAndSet(false))
+      if (clearSolution.getAndSet(false))
          children.clear();
 
-      if (newScanMeshView != null)
+      if (newScanMeshView != null && showSolution.get())
       {
          children.add(newScanMeshView);
       }
@@ -106,7 +115,7 @@ public class FusedSuperPixelImageViewer
 
    public void clear()
    {
-      clear.set(true);
+      clearSolution.set(true);
    }
 
    public Node getRoot()
