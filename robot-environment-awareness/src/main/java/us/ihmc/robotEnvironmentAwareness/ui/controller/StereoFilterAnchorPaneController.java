5842ddad530b5027d10500c7f1fd981a60a237b8
inhol <ilee@ihmc.us>
2019-08-02 17:07:40 -0500

    Added a tab for the stereo filter.

---
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/StereoFilterAnchorPaneController.java
@@ -0,0 +1,13 @@
+package us.ihmc.robotEnvironmentAwareness.ui.controller;
+
+public class StereoFilterAnchorPaneController extends REABasicUIController
+{
+
+   @Override
+   public void bindControls()
+   {
+      // TODO Auto-generated method stub
+      
+   }
+
+}
