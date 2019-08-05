cacf4bd2b0a43f6a471f7d6f2518a3c426f2a61f
Stefano <stefano.dafarra@gmail.com>
2019-08-05 17:46:50 -0500

    Using a specilized model in the collision manager tests. Added CollisionAvoidanceParameters doc.

--- atlas/src/test/java/us/ihmc/atlas/collisionAvoidance/AtlasCollisionAvoidanceTest.java
+++ atlas/src/test/java/us/ihmc/atlas/collisionAvoidance/AtlasCollisionAvoidanceTest.java
@@ -4,23 +4,22 @@ import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
+import us.ihmc.atlas.AtlasJointMap;
 import us.ihmc.atlas.AtlasRobotModel;
 import us.ihmc.atlas.AtlasRobotVersion;
+import us.ihmc.atlas.parameters.AtlasContactPointParameters;
+import us.ihmc.atlas.parameters.AtlasWalkingControllerParameters;
 import us.ihmc.avatar.collisionAvoidance.AvatarCollisionAvoidanceTest;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.commonWalkingControlModules.configurations.ShinCollisionAvoidanceParameters;
+import us.ihmc.commonWalkingControlModules.configurations.WalkingControllerParameters;
+import us.ihmc.robotics.robotSide.SideDependentList;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
-import us.ihmc.wholeBodyController.AdditionalSimulationContactPoints;
-import us.ihmc.wholeBodyController.FootContactPoints;
 
 public class AtlasCollisionAvoidanceTest extends AvatarCollisionAvoidanceTest
 {
-   FootContactPoints<RobotSide> simulationContactPoints = new AdditionalSimulationContactPoints<>(RobotSide.values, 2, 2, true, true);
-   private final AtlasRobotModel atlasRobotModel = new AtlasRobotModel(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS,
-                                                                       RobotTarget.SCS,
-                                                                       false,
-                                                                       simulationContactPoints);
+   private final AtlasRobotModel atlasRobotModel = new AtlasRobotModelForCollisionAvoidanceTest(AtlasRobotVersion.ATLAS_UNPLUGGED_V5_NO_HANDS, RobotTarget.SCS);
 
    @Test
    public void testWalkingUpOfMediumStep() throws SimulationExceededMaximumTimeException
@@ -71,5 +70,50 @@ public class AtlasCollisionAvoidanceTest extends AvatarCollisionAvoidanceTest
    public String getSimpleRobotName()
    {
       return "Atlas";
+   }
+   
+   public class AtlasRobotModelForCollisionAvoidanceTest extends AtlasRobotModel
+   {
+      public class EnabledShinCollisionAvoidanceParameters extends ShinCollisionAvoidanceParameters
+      {
+         @Override
+         public boolean useCollisionAvoidance()
+         {
+            return true;
+         }
+      }
+      
+      public class EnabledShinCollisionsWalkingControllerParameters extends AtlasWalkingControllerParameters
+      {
+         private final SideDependentList<ShinCollisionAvoidanceParameters> shinsCollisionAvoidanceParameters;
+
+         public EnabledShinCollisionsWalkingControllerParameters(RobotTarget target, AtlasJointMap jointMap, AtlasContactPointParameters contactPointParameters)
+         {
+            super(target, jointMap, contactPointParameters);
+            shinsCollisionAvoidanceParameters = new SideDependentList<ShinCollisionAvoidanceParameters>(new EnabledShinCollisionAvoidanceParameters(),
+                                                                                                        new EnabledShinCollisionAvoidanceParameters());
+         }
+         
+         public SideDependentList<ShinCollisionAvoidanceParameters> getShinsCollisionAvoidanceParameters()
+         {
+            return shinsCollisionAvoidanceParameters;
+         }
+         
+      }
+      
+      private final EnabledShinCollisionsWalkingControllerParameters walkingParameters;
+
+      public AtlasRobotModelForCollisionAvoidanceTest(AtlasRobotVersion atlasVersion, RobotTarget target)
+      {
+         super(atlasVersion, target);
+         walkingParameters = new EnabledShinCollisionsWalkingControllerParameters(target, super.getJointMap(), super.getContactPointParameters());
+      }
+      
+      @Override
+      public WalkingControllerParameters getWalkingControllerParameters()
+      {
+         return walkingParameters;
+      }
+      
    }
 }
