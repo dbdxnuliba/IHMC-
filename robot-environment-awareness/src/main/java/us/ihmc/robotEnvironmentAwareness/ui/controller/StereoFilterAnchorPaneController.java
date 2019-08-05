99150b75ace64f953cfd67b47b7be46741858c17
inhol <ilee@ihmc.us>
2019-08-05 12:24:48 -0500

    Added a tab for the stereo filter.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/StereoFilterAnchorPaneController.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/controller/StereoFilterAnchorPaneController.java
@@ -1,13 +1,22 @@
 package us.ihmc.robotEnvironmentAwareness.ui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
 public class StereoFilterAnchorPaneController extends REABasicUIController
 {
+   @FXML
+   private Slider stereoNumberOfBufferSlider;
+   @FXML
+   private Slider stereoBufferSizeSlider;
+   @FXML
+   private Slider surfaceNormalUpperBound;
+   @FXML
+   private Slider surfaceNormalLowerBound;
 
    @Override
    public void bindControls()
    {
-      // TODO Auto-generated method stub
       
    }
-
 }
