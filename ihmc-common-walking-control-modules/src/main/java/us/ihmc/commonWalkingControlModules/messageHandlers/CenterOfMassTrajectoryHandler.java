54e870cd82b8cb463deb4295d89f12d0b8de8d64
Stefano <stefano.dafarra@gmail.com>
2019-08-22 16:07:29 -0500

    Restored the CenterOfMassTrajectoryHandler has the CoM trajectory is also handled in the CoM height controller.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/messageHandlers/CenterOfMassTrajectoryHandler.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/messageHandlers/CenterOfMassTrajectoryHandler.java
@@ -115,48 +115,6 @@ public class CenterOfMassTrajectoryHandler extends EuclideanTrajectoryHandler
       return true;
    }
 
-   public boolean packDesiredCoMState(double controllerTime, FramePoint3D comPositionToPack, FrameVector3D comVelocityToPack,
-                                      FrameVector3D comAccelerationToPack)
-   {
-      if (!isWithinInterval(controllerTime))
-      {
-         if (comPositionToPack != null)
-         {
-            comPositionToPack.setToNaN(ReferenceFrame.getWorldFrame());
-         }
-
-         if (comVelocityToPack != null)
-         {
-            comVelocityToPack.setToNaN(ReferenceFrame.getWorldFrame());
-         }
-         
-         if (comAccelerationToPack != null)
-         {
-            comAccelerationToPack.setToNaN(ReferenceFrame.getWorldFrame());
-         }
-         
-         return false;
-      }
-
-      if (comPositionToPack != null)
-      {
-         comPositionToPack.setIncludingFrame(ReferenceFrame.getWorldFrame(), getPosition());
-         comPositionToPack.add(offset);
-      }
-
-      if (comVelocityToPack != null)
-      {
-         comVelocityToPack.setIncludingFrame(ReferenceFrame.getWorldFrame(), getVelocity());
-      }
-      
-      if (comAccelerationToPack != null)
-      {
-         comAccelerationToPack.setIncludingFrame(ReferenceFrame.getWorldFrame(), getAcceleration());
-      }
-
-      return true;
-   }
-
    public void setPositionOffset(FrameVector3DReadOnly offset)
    {
       this.offset.setIncludingFrame(offset);
