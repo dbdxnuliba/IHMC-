d3134142e7d519687d8bacd61d1ccc49980d1d6a
inhol <ilee@ihmc.us>
2019-10-01 16:17:55 -0500

    Added preserve mode for stereo viewer.

--- robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/graphicsBuilders/StereoVisionPointCloudViewer.java
+++ robot-environment-awareness/src/main/java/us/ihmc/robotEnvironmentAwareness/ui/graphicsBuilders/StereoVisionPointCloudViewer.java
@@ -6,9 +6,14 @@ import java.util.concurrent.atomic.AtomicReference;
 import controller_msgs.msg.dds.StereoVisionPointCloudMessage;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.MeshView;
+import javafx.scene.transform.Affine;
 import us.ihmc.communication.packets.MessageTools;
+import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Point3D32;
+import us.ihmc.euclid.tuple4D.Quaternion;
 import us.ihmc.graphicsDescription.MeshDataGenerator;
+import us.ihmc.javaFXToolkit.JavaFXTools;
+import us.ihmc.javaFXToolkit.shapes.JavaFXCoordinateSystem;
 import us.ihmc.messager.MessagerAPIFactory.Topic;
 import us.ihmc.messager.SharedMemoryMessager;
 import us.ihmc.robotEnvironmentAwareness.communication.REAModuleAPI;
@@ -18,17 +23,31 @@ import us.ihmc.robotEnvironmentAwareness.ui.controller.PointCloudAnchorPaneContr
 public class StereoVisionPointCloudViewer extends AbstractSourceViewer<StereoVisionPointCloudMessage>
 {
    private final AtomicReference<Integer> sizeOfPointCloud;
+   private final AtomicReference<Boolean> enablePreserve = new AtomicReference<Boolean>(false);
+
+   private final double LENGTH_OF_SENSOR_FRAME = 0.3;
+   private final JavaFXCoordinateSystem sensorCoordinateSystem;
+   private final Affine sensorPose = new Affine();
+   private final AtomicReference<Affine> lastAffine = new AtomicReference<>();
 
    public StereoVisionPointCloudViewer(Topic<StereoVisionPointCloudMessage> messageState, REAUIMessager uiMessager)
    {
       super(messageState, uiMessager);
       sizeOfPointCloud = uiMessager.createInput(REAModuleAPI.UIStereoVisionSize, PointCloudAnchorPaneController.initialSizeOfPointCloud);
+      sensorCoordinateSystem = new JavaFXCoordinateSystem(LENGTH_OF_SENSOR_FRAME);
+      sensorCoordinateSystem.getTransforms().add(sensorPose);
+
+      children.add(sensorCoordinateSystem);
    }
 
    public StereoVisionPointCloudViewer(Topic<StereoVisionPointCloudMessage> messageState, SharedMemoryMessager uiMessager)
    {
       super(messageState, uiMessager);
       sizeOfPointCloud = uiMessager.createInput(REAModuleAPI.UIStereoVisionSize, PointCloudAnchorPaneController.initialSizeOfPointCloud);
+      sensorCoordinateSystem = new JavaFXCoordinateSystem(0.1);
+      sensorCoordinateSystem.getTransforms().add(sensorPose);
+
+      children.add(sensorCoordinateSystem);
    }
 
    public void render()
@@ -40,19 +59,34 @@ public class StereoVisionPointCloudViewer extends AbstractSourceViewer<StereoVis
 
       if (!enable.get())
          return;
-
+      
       if (newScanMeshView != null)
       {
-         children.clear();
+         if (!enablePreserve.get())
+            children.clear();
          children.add(newScanMeshView);
+         
+         Affine affine = lastAffine.getAndSet(null);
+         if (affine != null)
+         {
+            sensorPose.setToTransform(affine);
+            JavaFXCoordinateSystem sensorCoordinateSystem = new JavaFXCoordinateSystem(LENGTH_OF_SENSOR_FRAME);
+            sensorCoordinateSystem.getTransforms().add(affine);
+            children.add(sensorCoordinateSystem);
+         }
       }
    }
 
    @Override
    public void unpackPointCloud(StereoVisionPointCloudMessage message)
    {
+      Quaternion orientation = message.getSensorOrientation();
+      Point3D position = message.getSensorPosition();
+      lastAffine.set(JavaFXTools.createAffineFromQuaternionAndTuple(orientation, position));
+
       Point3D32 scanPoint = new Point3D32();
       meshBuilder.clear();
+
       int numberOfScanPoints = message.getPointCloud().size() / 3;
       int sizeOfPointCloudToVisualize = Math.min(numberOfScanPoints, sizeOfPointCloud.get());
 
@@ -89,6 +123,11 @@ public class StereoVisionPointCloudViewer extends AbstractSourceViewer<StereoVis
    protected Topic<Boolean> createClearInput()
    {
       return REAModuleAPI.UIStereoVisionClear;
+   }
+
+   public void enablePreserve(boolean enable)
+   {
+      enablePreserve.set(enable);
    }
 
    public static javafx.scene.paint.Color intToColor(int value)
